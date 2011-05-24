8dea151038fec7fee61a4fa9b193fdfccdc0c05c
David Dollar <ddollar@gmail.com>
2011-05-24 12:28:13 -0400

    create generic database yml

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -33,6 +33,7 @@ class LanguagePack::Ruby < LanguagePack::Base
     setup_language_pack_environment
     git_dir = ENV.delete("GIT_DIR") # can mess with bundler
     build_bundler
+    create_database_yml
     run_compile_hook
     ENV["GIT_DIR"] = git_dir
   end
@@ -102,6 +103,57 @@ private
       else
         error "Failed to install gems via Bundler."
       end
+    end
+  end
+
+  def create_database_yml
+    return unless File.directory?("config")
+    File.open("config/database.yml", "w") do |file|
+      file.puts <<-DATABASE_YML
+<%
+
+require 'cgi'
+require 'uri'
+
+begin
+  uri = URI.parse(ENV["DATABASE_URL"])
+rescue URI::InvalidURIError
+  raise "Invalid DATABASE_URL"
+end
+
+raise "No RACK_ENV or RAILS_ENV found" unless ENV["RAILS_ENV"] || ENV["RACK_ENV"]
+
+def attribute(name, value)
+  value ? "\#{name}: \#{value}" : ""
+end
+
+adapter = uri.scheme
+adapter = "postgresql" if adapter == "postgres"
+
+database = (uri.path || "").split("/")[1]
+
+username = uri.user
+password = uri.password
+
+host = uri.host
+port = uri.port
+
+params = CGI.parse(uri.query || "")
+
+%>
+
+<%= ENV["RAILS_ENV"] || ENV["RACK_ENV"] %>:
+  <%= attribute "adapter",  adapter %>
+  <%= attribute "database", database %>
+  <%= attribute "username", username %>
+  <%= attribute "password", password %>
+  <%= attribute "host",     host %>
+  <%= attribute "port",     port %>
+
+<% params.each do |key, value| %>
+  <%= key %>: <%= value.first %>
+<% end %>
+      DATABASE_YML
     end
   end
 
