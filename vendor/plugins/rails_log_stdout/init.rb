6b76b6c693a6c209f11614670ce7c4b313deead4
David Dollar <ddollar@gmail.com>
2011-06-09 11:48:51 -0700

    merge new rails_log_stdout

--- vendor/plugins/rails_log_stdout/init.rb
+++ vendor/plugins/rails_log_stdout/init.rb
@@ -1,26 +1,21 @@
 STDOUT.sync = true
 
-case Rails::VERSION::MAJOR
-
-when 3 then
+def Rails.heroku_stdout_logger
   logger = Logger.new(STDOUT)
-  logger.level = Logger::INFO
-  Rails.logger = Rails.application.config.logger = logger
+  logger.level = Logger.const_get(([ENV['LOG_LEVEL'].to_s.upcase, "INFO"] & %w[DEBUG INFO WARN ERROR FATAL UNKNOWN]).compact.first)
+  logger
+end
 
-when 2 then
-  # redefine Rails.logger
-  def Rails.logger
-    @@logger ||= begin
-      logger = Logger.new(STDOUT)
-      logger.level = Logger::INFO
-      logger
+case Rails::VERSION::MAJOR
+  when 3 then Rails.logger = Rails.application.config.logger = Rails.heroku_stdout_logger
+  when 2 then
+    # redefine Rails.logger
+    def Rails.logger
+      @@logger ||= Rails.heroku_stdout_logger
     end
-  end
-
-  # borrowed from Rails::Initializer#initialize_framework_logging
-  ([ :active_record, :action_controller, :action_mailer ] & Rails.configuration.frameworks).each do |framework|
-    framework.to_s.camelize.constantize.const_get("Base").logger = Rails.logger
-  end
-  ActiveSupport::Dependencies.logger = Rails.logger
-  Rails.cache.logger = Rails.logger
+    # borrowed from Rails::Initializer#initialize_framework_logging
+    [ActiveSupport::Dependencies, Rails.cache].concat(
+      ([:active_record, :action_controller, :action_mailer] & Rails.configuration.frameworks)\
+        .map { |framework| framework.to_s.camelize.constantize.const_get("Base") }
+    ).each { |k| k.logger = Rails.logger }
 end
