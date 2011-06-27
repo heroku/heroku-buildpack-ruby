608586b3fc83f600f16964a5a49ffbb0653a9e72
David Dollar <ddollar@gmail.com>
2011-06-27 12:00:07 -0400

    update rails_log_stdout

--- vendor/plugins/rails_log_stdout/init.rb
+++ vendor/plugins/rails_log_stdout/init.rb
@@ -1,21 +1,40 @@
-STDOUT.sync = true
+begin
 
-def Rails.heroku_stdout_logger
-  logger = Logger.new(STDOUT)
-  logger.level = Logger.const_get(([ENV['LOG_LEVEL'].to_s.upcase, "INFO"] & %w[DEBUG INFO WARN ERROR FATAL UNKNOWN]).compact.first)
-  logger
-end
+  STDOUT.sync = true
 
-case Rails::VERSION::MAJOR
-  when 3 then Rails.logger = Rails.application.config.logger = Rails.heroku_stdout_logger
-  when 2 then
-    # redefine Rails.logger
-    def Rails.logger
-      @@logger ||= Rails.heroku_stdout_logger
-    end
-    # borrowed from Rails::Initializer#initialize_framework_logging
-    [ActiveSupport::Dependencies, Rails.cache].concat(
-      ([:active_record, :action_controller, :action_mailer] & Rails.configuration.frameworks)\
-        .map { |framework| framework.to_s.camelize.constantize.const_get("Base") }
-    ).each { |k| k.logger = Rails.logger }
+  def Rails.heroku_stdout_logger
+    logger = Logger.new(STDOUT)
+    logger.level = Logger.const_get(([ENV['LOG_LEVEL'].to_s.upcase, "INFO"] & %w[DEBUG INFO WARN ERROR FATAL UNKNOWN]).compact.first)
+    logger
+  end
+
+  case Rails::VERSION::MAJOR
+    when 3 then Rails.logger = Rails.application.config.logger = Rails.heroku_stdout_logger
+    when 2 then
+      # redefine Rails.logger
+      def Rails.logger
+        @@logger ||= Rails.heroku_stdout_logger
+      end
+      %w(
+        ActiveSupport::Dependencies
+        ActiveRecord::Base
+        ActionController::Base
+        ActionMailer::Base
+        ActionView::Base
+        ActiveResource::Base
+      ).each do |klass_name|
+        begin
+          klass = Object
+          klass_name.split("::").each { |part| klass = klass.const_get(part) }
+          klass.logger = Rails.logger
+        rescue
+        end
+      end
+      Rails.cache.logger = Rails.logger rescue nil
+  end
+
+rescue Exception => ex
+
+  puts "WARNING: Exception during rails_log_stdout init: #{ex.message}"
+
 end
