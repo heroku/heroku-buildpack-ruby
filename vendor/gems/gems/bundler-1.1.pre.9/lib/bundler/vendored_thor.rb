4a4abbfb08d97b979516a3a2cf806b76f2c98d52
Terence Lee <hone02@gmail.com>
2011-09-26 14:11:49 -0500

    remove vendored gems since they're on s3

--- vendor/gems/gems/bundler-1.1.pre.9/lib/bundler/vendored_thor.rb
+++
@@ -1,7 +0,0 @@
-if defined?(Thor)
-  Bundler.ui.warn "Thor has already been required. " +
-    "This may cause Bundler to malfunction in unexpected ways."
-end
-$:.unshift File.expand_path('../vendor', __FILE__)
-require 'thor'
-require 'thor/actions'
