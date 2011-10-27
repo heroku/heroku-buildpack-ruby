494f2c057797ae1e3ebe34f0b33738997f3bb765
Terence Lee <hone02@gmail.com>
2011-10-27 09:47:35 -0500

    Revert "remove 1.9 style encoding setting"
    This reverts commit 31b6f8f9d460e6f9e63fa77bc7ca45263666dfa7.

--- lib/language_pack/base.rb
+++ lib/language_pack/base.rb
@@ -3,6 +3,8 @@ require "pathname"
 require "yaml"
 require "digest/sha1"
 
+Encoding.default_external = Encoding::UTF_8
+
 # abstract class that all the Ruby based Language Packs inherit from
 class LanguagePack::Base
   VENDOR_URL = "https://s3.amazonaws.com/heroku-buildpack-ruby"
