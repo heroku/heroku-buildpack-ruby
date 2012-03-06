c1755383b76658e2ab50dc70cb6e7847a6147810
Jonathan Dance <jd@wuputah.com>
2012-03-06 15:06:56 -0800

    dont install plugins if plugins exist as gems

--- lib/language_pack/rails2.rb
+++ lib/language_pack/rails2.rb
@@ -63,7 +63,7 @@ private
   # @param [String] name of the plugin
   def install_plugin(name)
     plugin_dir = "vendor/plugins/#{name}"
-    return if File.exist?(plugin_dir)
+    return if File.exist?(plugin_dir) || gem_is_bundled?(name)
     puts "Injecting #{name}"
     FileUtils.mkdir_p plugin_dir
     Dir.chdir(plugin_dir) do |dir|
@@ -78,4 +78,3 @@ private
     ['shared-database:5mb']
   end
 end
-
