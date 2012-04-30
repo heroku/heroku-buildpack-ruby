c1be6f1f0dc4b94be85d24427307d7058d8f9fb8
Alex Grintsvayg <alex@flickswitch.cc>
2012-04-30 17:49:44 -0400

    output error info for debugging

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -456,7 +456,7 @@ params = CGI.parse(uri.query || "")
 
   def generate_jekyll_site
     puts "Building jekyll site"
-    run("env PATH=$PATH bundle exec jekyll")
+    run("env PATH=$PATH bundle exec jekyll 2>&1")
     unless $? == 0
       error "Failed to generate site with jekyll."
     end
