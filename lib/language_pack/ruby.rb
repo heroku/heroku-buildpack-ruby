95a69cdebe7d63d6d0c6446b0cfa67a9f7653820
Terence Lee <hone02@gmail.com>
2012-09-19 14:00:20 +0300

    pipe bundle clean output

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -401,7 +401,7 @@ ERROR
       if $?.success?
         log "bundle", :status => "success"
         puts "Cleaning up the bundler cache."
-        run "bundle clean"
+        pipe "bundle clean"
         cache_store ".bundle"
         cache_store "vendor/bundle"
 
