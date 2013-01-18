ba1f8113ab40b00f6697252b54ce89ba93a5062e
Terence Lee <hone02@gmail.com>
2013-01-18 14:46:00 -0800

    display the ruby version change to the user

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -606,6 +606,8 @@ params = CGI.parse(uri.query || "")
       purge_bundler_cache
     elsif cache_exists?(bundler_cache) && !(File.exists?(ruby_version_cache) && full_ruby_version == File.read(ruby_version_cache).chomp)
       puts "Ruby version change detected. Clearing bundler cache."
+      puts "Old: #{File.read(ruby_version_cache).chomp}"
+      puts "New: #{full_ruby_version}"
       purge_bundler_cache
     end
 
