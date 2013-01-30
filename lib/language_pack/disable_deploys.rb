d9c61d49e518c4964cec38e0ba176ecaa52c637d
Terence Lee <hone02@gmail.com>
2013-01-30 11:21:10 -0800

    update copy

--- lib/language_pack/disable_deploys.rb
+++ lib/language_pack/disable_deploys.rb
@@ -11,7 +11,7 @@ class LanguagePack::DisableDeploys < LanguagePack::Base
   end
 
   def compile
-    error "Ruby deploys have been temporarily disabled. Please see https://status.heroku.com/incidents/489 for more info and a workaround if you need to deploy."
+    error "Ruby deploys have been temporarily disabled due to a Rubygems.org security breach.\nPlease see https://status.heroku.com/incidents/489 for more info and a workaround if you need to deploy."
   end
 end
 
