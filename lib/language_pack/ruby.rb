e69301b54de1899c981a1e1b37787e4b664c2ed0
Terence Lee <hone02@gmail.com>
2013-01-30 11:51:43 -0800

    use bundle install local

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -362,7 +362,7 @@ ERROR
   def build_bundler
     log("bundle") do
       bundle_without = ENV["BUNDLE_WITHOUT"] || "development:test"
-      bundle_command = "bundle install --without #{bundle_without} --path vendor/bundle --binstubs vendor/bundle/bin"
+      bundle_command = "bundle install --without #{bundle_without} --path vendor/bundle --binstubs vendor/bundle/bin --local"
 
       unless File.exist?("Gemfile.lock")
         error "Gemfile.lock is required. Please run \"bundle install\" locally\nand commit your Gemfile.lock."
