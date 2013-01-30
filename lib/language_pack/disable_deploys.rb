4531e5a42a096069ae5dc9714843477babf80cbd
Terence Lee <hone02@gmail.com>
2013-01-30 11:17:23 -0800

    update error message to link to status

--- lib/language_pack/disable_deploys.rb
+++ lib/language_pack/disable_deploys.rb
@@ -11,7 +11,7 @@ class LanguagePack::DisableDeploys < LanguagePack::Base
   end
 
   def compile
-    error "Ruby deploys have been temporarily disabled. We will have more information available shortly, including a workaround."
+    error "Ruby deploys have been temporarily disabled. Please see https://status.heroku.com/incidents/489 for more info and a workaround if you need to deploy."
   end
 end
 
