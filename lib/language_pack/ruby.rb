91b81406ba8fa36c9ff6a018ff44cb0e790665d7
Matt Manning <matt.manning@gmail.com>
2013-03-03 16:06:15 -0800

    Patch from @aJanuary. Override jekyll server and autorebuilding in _config.yml

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -597,7 +597,7 @@ params = CGI.parse(uri.query || "")
 
   def generate_jekyll_site
     puts "Building jekyll site"
-    pipe("env PATH=$PATH bundle exec jekyll 2>&1")
+    pipe("env PATH=$PATH bundle exec jekyll --no-server --no-auto 2>&1")
     unless $? == 0
       error "Failed to generate site with jekyll."
     end
