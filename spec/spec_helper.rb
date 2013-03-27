bdd6883145791af7b7812eb8e258103de44c3ed4
Terence Lee <hone02@gmail.com>
2013-03-27 16:22:47 -0500

    move anvil cleanup into hatchet

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,9 +9,6 @@ RSpec.configure do |config|
   config.filter_run :focused => true
   config.run_all_when_everything_filtered = true
   config.alias_example_to :fit, :focused => true
-  config.after(:each) {
-    `find -name .anvil`.split("\n").each {|dir| FileUtils.rm_rf(dir) }
-  }
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
