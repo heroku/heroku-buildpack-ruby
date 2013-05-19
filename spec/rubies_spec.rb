698911680f302a1737a04e1ed1b8b6cd157b157e
schneems <richard.schneeman@gmail.com>
2013-05-19 13:19:46 -0500

    Setting up travis testing

--- spec/rubies_spec.rb
+++ spec/rubies_spec.rb
@@ -2,37 +2,32 @@ require_relative 'spec_helper'
 
 describe "Ruby Versions" do
   it "should deploy ruby 1.8.7 properly" do
-    Hatchet::AnvilApp.new("mri_187", :buildpack => buildpack).deploy do |app, heroku, output|
-      expect(app).to be_deployed
-      expect(successful_body(app)).to match("ruby 1.8.7")
+    Hatchet::AnvilApp.new("mri_187").deploy do |app, heroku, output|
+      expect(app.run('ruby -v')).to match("1.8.7")
     end
   end
 
   it "should deploy ruby 1.9.2 properly" do
-    Hatchet::AnvilApp.new("mri_192", :buildpack => buildpack).deploy do |app, heroku, output|
-      expect(app).to be_deployed
-      expect(successful_body(app)).to match("ruby 1.9.2")
+    Hatchet::AnvilApp.new("mri_192").deploy do |app, heroku, output|
+      expect(app.run('ruby -v')).to match("1.9.2")
     end
   end
 
   it "should deploy ruby 1.9.2 properly (git)" do
-    Hatchet::GitApp.new("mri_192", :buildpack => git_repo).deploy do |app, heroku, output|
-      expect(app).to be_deployed
-      expect(successful_body(app)).to match("ruby 1.9.2")
+    Hatchet::GitApp.new("mri_192", buildpack: git_repo).deploy do |app, heroku, output|
+      expect(app.run('ruby -v')).to match("1.9.2")
     end
   end
 
   it "should deploy ruby 1.9.3 properly" do
-    Hatchet::AnvilApp.new("mri_193", :buildpack => buildpack).deploy do |app, heroku, output|
-      expect(app).to be_deployed
-      expect(successful_body(app)).to match("ruby 1.9.3")
+    Hatchet::AnvilApp.new("mri_193").deploy do |app, heroku, output|
+      expect(app.run('ruby -v')).to match("1.9.3")
     end
   end
 
   it "should deploy ruby 2.0.0 properly" do
-    Hatchet::AnvilApp.new("mri_200", :buildpack => buildpack).deploy do |app, heroku|
-      expect(app).to be_deployed
-      expect(successful_body(app)).to match("ruby 2.0.0")
+    Hatchet::AnvilApp.new("mri_200").deploy do |app, heroku|
+      expect(app.run('ruby -v')).to match("2.0.0")
     end
   end
 end
