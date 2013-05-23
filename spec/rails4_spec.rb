e6bd38920c8d74055bd978a6a3293be6f9163cdb
schneems <richard.schneeman@gmail.com>
2013-05-23 14:24:47 -0500

    Auto Retry deploy failures 3 times on travis
    When a deploy fails hatchet will now retry a deploy HATCHET_RETRIES times before raising an error. This mitigates ghosts in the machine and makes the test suite's red/green status more truthy.

--- spec/rails4_spec.rb
+++ spec/rails4_spec.rb
@@ -2,9 +2,9 @@ require_relative 'spec_helper'
 
 describe "Rails 4.x" do
   it "should deploy on ruby 1.9.3" do
-    Hatchet::AnvilApp.new("rails4-manifest").deploy do |app, heroku, output|
+    Hatchet::AnvilApp.new("rails4-manifest").deploy do |app, heroku|
       add_database(app, heroku)
-      expect(output).to include("Detected manifest file, assuming assets were compiled locally")
+      expect(app.output).to include("Detected manifest file, assuming assets were compiled locally")
     end
   end
 
