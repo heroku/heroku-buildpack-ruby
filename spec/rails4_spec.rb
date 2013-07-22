4d28b07bf8124561a54d863987dde1f4cfaf94c6
schneems <richard.schneeman@gmail.com>
2013-07-22 15:55:39 -0500

    Use Hatchet::Runner available in 1.1.0
    Fix tests on travis

--- spec/rails4_spec.rb
+++ spec/rails4_spec.rb
@@ -2,14 +2,15 @@ require_relative 'spec_helper'
 
 describe "Rails 4.x" do
   it "should deploy on ruby 1.9.3" do
-    Hatchet::AnvilApp.new("rails4-manifest").deploy do |app, heroku|
+    Hatchet::Runner.new("rails4-manifest").deploy do |app, heroku|
       add_database(app, heroku)
       expect(app.output).to include("Detected manifest file, assuming assets were compiled locally")
     end
   end
 
   it "upgraded from 3 to 4 missing ./bin still works" do
-    Hatchet::AnvilApp.new("rails3-to-4-no-bin").deploy do |app, heroku|
+    Hatchet::Runner.new("rails3-to-4-no-bin").deploy do |app, heroku|
+      expect(app.output).to include("Asset precompilation completed")
       add_database(app, heroku)
 
       app.run("rails console") do |console|
--- spec/rubies_spec.rb
+++ spec/rubies_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe "Ruby Versions" do
   it "should deploy ruby 1.8.7 properly" do
-    Hatchet::AnvilApp.new("mri_187").deploy do |app|
+    Hatchet::Runner.new("mri_187").deploy do |app|
       version = '1.8.7'
       expect(app.output).to match(version)
       expect(app.run('ruby -v')).to match(version)
@@ -10,7 +10,7 @@ describe "Ruby Versions" do
   end
 
   it "should deploy ruby 1.9.2 properly" do
-    Hatchet::AnvilApp.new("mri_192").deploy do |app|
+    Hatchet::Runner.new("mri_192").deploy do |app|
       version = '1.9.2'
       expect(app.output).to match(version)
       expect(app.run('ruby -v')).to match(version)
@@ -26,7 +26,7 @@ describe "Ruby Versions" do
   end
 
   it "should deploy ruby 1.9.3 properly" do
-    Hatchet::AnvilApp.new("mri_193").deploy do |app|
+    Hatchet::Runner.new("mri_193").deploy do |app|
       version = '1.9.3'
       expect(app.output).to match(version)
       expect(app.run('ruby -v')).to match(version)
@@ -34,7 +34,7 @@ describe "Ruby Versions" do
   end
 
   it "should deploy ruby 2.0.0 properly" do
-    Hatchet::AnvilApp.new("mri_200").deploy do |app|
+    Hatchet::Runner.new("mri_200").deploy do |app|
       version = '2.0.0'
       expect(app.output).to match(version)
       expect(app.run('ruby -v')).to match(version)
