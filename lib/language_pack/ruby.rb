fd4dfc016f360d08b27be93c0a38b56cb06e4249
Terence Lee <hone02@gmail.com>
2013-08-28 20:07:51 -0400

    v78

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -11,7 +11,7 @@ class LanguagePack::Ruby < LanguagePack::Base
   extend LanguagePack::BundlerLockfile::ClassMethods
 
   NAME                 = "ruby"
-  BUILDPACK_VERSION    = "v77"
+  BUILDPACK_VERSION    = "v78"
   LIBYAML_VERSION      = "0.1.4"
   LIBYAML_PATH         = "libyaml-#{LIBYAML_VERSION}"
   BUNDLER_VERSION      = "1.3.2"
