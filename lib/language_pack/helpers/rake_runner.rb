c1e051914661ec275406f26985c103ec20bec09b
schneems <richard.schneeman@gmail.com>
2013-12-13 14:13:46 -0800

    use options hashes for RakeRunner

--- lib/language_pack/helpers/rake_runner.rb
+++ lib/language_pack/helpers/rake_runner.rb
@@ -5,20 +5,17 @@ class LanguagePack::Helpers::RakeRunner
     ALLOWED = [:pass, :fail, :no_load, :not_found]
     include LanguagePack::ShellHelpers
 
-    attr_accessor :output, :time, :command, :status, :task_defined, :rakefile_can_load
+    attr_accessor :output, :time, :task, :status, :task_defined, :rakefile_can_load
 
     alias :rakefile_can_load? :rakefile_can_load
     alias :task_defined?      :task_defined
     alias :is_defined?        :task_defined
 
-    def initialize(task, command = nil)
-      @task    = task
-      command  = "env PATH=$PATH:bin bundle exec rake #{task} 2>&1" if command.nil?
-      raise "expect #{command} to contain #{task}" unless command.include?(task)
-
-      @command = command
-      @status  = :nil
-      @output  = ""
+    def initialize(task, options = {})
+      @task            = task
+      @default_options = {user_env: true}.merge(options)
+      @status          = :nil
+      @output          = ""
     end
 
     def success?
@@ -35,11 +32,11 @@ class LanguagePack::Helpers::RakeRunner
       @status
     end
 
-    def invoke(cmd = nil)
-      cmd = cmd || @command
-      puts "Running: rake #{@task}"
+    def invoke(options = {})
+      options = @default_options.merge(options)
+      puts "Running: rake #{task}"
       time = Benchmark.realtime do
-        self.output = pipe(cmd)
+        self.output = pipe(task, options)
       end
       self.time = time
 
@@ -104,15 +101,15 @@ class LanguagePack::Helpers::RakeRunner
     !task_defined?(task)
   end
 
-  def task(rake_task, command = nil)
-    t = RakeTask.new(rake_task, command)
+  def task(rake_task, options = {})
+    t = RakeTask.new(rake_task, options)
     t.task_defined      = task_defined?(rake_task)
     t.rakefile_can_load = rakefile_can_load?
     t
   end
 
-  def invoke(task, command = nil)
-    self.task(task, command).invoke
+  def invoke(task, options = {})
+    self.task(task, options).invoke
   end
 
 private
