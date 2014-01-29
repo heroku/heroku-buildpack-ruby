d0a30610d64833c1c06afed94ccdff2221b1eccd
schneems <richard.schneeman@gmail.com>
2014-01-29 12:37:22 -0600

    show database URL when failure

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -594,7 +594,7 @@ require 'uri'
 begin
   uri = URI.parse(ENV["DATABASE_URL"])
 rescue URI::InvalidURIError
-  raise "Invalid DATABASE_URL"
+  raise "Invalid DATABASE_URL: #{ENV['DATABASE_URL'].inspect}"
 end
 
 raise "No RACK_ENV or RAILS_ENV found" unless ENV["RAILS_ENV"] || ENV["RACK_ENV"]
