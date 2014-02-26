6a8a5574328ee902765d4b86fc6936ff9257bf62
schneems <richard.schneeman@gmail.com>
2014-02-26 13:08:01 -0600

    fix tests

--- spec/helpers/shell_spec.rb
+++ spec/helpers/shell_spec.rb
@@ -7,17 +7,17 @@ end
 
 
 describe "ShellHelpers" do
-  fit "format ugly keys correctly" do
+  it "format ugly keys correctly" do
     env      = {%Q{ un"matched } => "bad key"}
     result   = FakeShell.new.command_options_to_string("bundle install", env:  env)
-    expected = %Q{ env \\ un\\\"matched\\ =\"bad\\ key\" bash -c bundle\\ install }
+    expected = %Q{ env \\ un\\\"matched\\ =bad\\ key bash -c bundle\\ install 2>&1 }
     expect(result.strip).to eq(expected.strip)
   end
 
-  fit "formats ugly values correctly" do
+  it "formats ugly values correctly" do
     env      = {"BAD VALUE"      => %Q{ )(*&^%$#'$'\n''@!~\'\ }}
     result   = FakeShell.new.command_options_to_string("bundle install", env:  env)
-    expected = %Q{ env BAD\\ VALUE=\"\\ \\)\\(\\*\\&\\^\\%\\$\\#\\'\\$\\''\n'\\'\\'@\\!\\~\\'\\ \" bash -c bundle\\ install }
+    expected = %Q{ env BAD\\ VALUE=\\ \\)\\(\\*\\&\\^\\%\\$\\#\\'\\$\\''\n'\\'\\'@\\!\\~\\'\\  bash -c bundle\\ install 2>&1 }
     expect(result.strip).to eq(expected.strip)
   end
 end
