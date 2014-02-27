bce225c1e10492709c9c1c23d3a9eb87a290d1e0
Terence Lee <hone02@gmail.com>
2014-02-26 17:56:45 -0800

    use system env

--- lib/language_pack/shell_helpers.rb
+++ lib/language_pack/shell_helpers.rb
@@ -84,7 +84,7 @@ module LanguagePack
       options[:out] ||= "2>&1"
       options[:env] = user_env_hash.merge(options[:env]) if options[:user_env]
       env = options[:env].map {|key, value| "#{key.shellescape}=#{value.shellescape}" }.join(" ")
-      "env #{env} bash -c #{command.shellescape} #{options[:out]} "
+      "/usr/bin/env #{env} bash -c #{command.shellescape} #{options[:out]} "
     end
 
     # run a shell command and stream the output
