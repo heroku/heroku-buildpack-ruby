c0626aba2cbb93e722e6619f62a988e8536534df
Aaron Triantafyllidis and Jordi Noguera <pair+atriantafyllidis+jordi@pivotallabs.com>
2014-05-16 15:10:41 -0400

    Upgrade reference to Machete, no longer use buildpack uploader

--- cf_spec/cf_spec_helper.rb
+++ cf_spec/cf_spec_helper.rb
@@ -3,11 +3,3 @@ require 'machete'
 
 `mkdir -p log`
 Machete.logger = Machete::Logger.new("log/integration.log")
-
-RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-
-  config.before(:suite) do
-    Machete::BuildpackUploader.new(:ruby)
-  end
-end
