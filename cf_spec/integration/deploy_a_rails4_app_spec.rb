c76da8d5ddf78700d9d274147673a7eb490cd129
Eric Hul ehu and Jordi Noguera <pair+eric+jordi@pivotallabs.com>
2014-05-19 17:45:57 -0400

    default way of starting rails 4 app is bin/rails

--- cf_spec/integration/deploy_a_rails4_app_spec.rb
+++ cf_spec/integration/deploy_a_rails4_app_spec.rb
@@ -4,7 +4,7 @@ require "cf_spec_helper"
 describe 'deploying a rails 4 application', :ruby_buildpack do
   it 'make the homepage available' do
     Machete.deploy_app("rails4_web_app", :ruby, {
-      cmd: "bundle exec rake db:migrate && bundle exec rails s -p $PORT",
+      cmd: "bundle exec rake db:migrate && bundle exec bin/rails s -p $PORT",
       with_pg: true
     }) do |app|
       expect(app).to be_staged
--- cf_spec/integration/deploy_a_rails_latest_app_spec.rb
+++ cf_spec/integration/deploy_a_rails_latest_app_spec.rb
@@ -4,7 +4,7 @@ require "cf_spec_helper"
 describe 'deploying a rails 4 application', :ruby_buildpack do
   it 'make the homepage available' do
     Machete.deploy_app("rails_latest_web_app", :ruby, {
-      cmd: "bundle exec rake db:migrate && bundle exec rails s -p $PORT",
+      cmd: "bundle exec rake db:migrate && bundle exec bin/rails s -p $PORT",
       with_pg: true,
       vendor_gems_before_push: true
     }) do |app|
