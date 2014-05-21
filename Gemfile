c3ac8e26955d1ff652f7348041c110ab25d6320b
Aaron Triantafyllidis and Eric Hu <pair+atriantafyllidis+ehu@pivotallabs.com>
2014-05-21 09:48:52 -0400

    Use base packager gem for packaging

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source "https://rubygems.org"
 
-gem 'pry'
-gem 'rubyzip'
+gem 'base_packager', git: "https://github.com/cf-buildpacks/base_packager.git", ref: "3b54ae2"
 gem "heroku_hatchet"
 gem "rspec-core"
 gem "rspec-expectations"
--- lib/buildpack_packager.rb
+++ lib/buildpack_packager.rb
@@ -1,96 +1,58 @@
-require 'zip'
+require 'base_packager'
+require 'json'
 
-module BuildpackPackager
-  DEPENDENCIES = [
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/node-0.4.7.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/libyaml-0.1.6.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/bundler-1.5.2.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby_versions.yml',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.1.1.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.1.0.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.2.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-build-1.8.7.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.1.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.1.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.2.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.2.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.3.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.3.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.4.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.4.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.5.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.5.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0-jruby-1.7.5.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.6.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.6.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0-jruby-1.7.6.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.8.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.8.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0-jruby-1.7.8.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.9.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.9.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0-jruby-1.7.9.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.10.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.10.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0-jruby-1.7.10.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.11.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.11.tgz',
-    'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0-jruby-1.7.11.tgz',
-    'http://heroku-jdk.s3.amazonaws.com/openjdk1.7.0_25.tar.gz',
-    'http://heroku-jdk.s3.amazonaws.com/openjdk1.7.0_45.tar.gz',
-    'http://heroku-jdk.s3.amazonaws.com/openjdk7-latest.tar.gz'
-  ]
+class BuildpackPackager < BasePackager
+  def dependencies
+    [
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/node-0.4.7.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/libyaml-0.1.6.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/bundler-1.5.2.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby_versions.yml',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.1.1.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.1.0.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.2.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-build-1.8.7.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.1.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.1.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.2.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.2.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.3.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.3.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.4.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.4.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.5.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.5.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0-jruby-1.7.5.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.6.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.6.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0-jruby-1.7.6.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.8.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.8.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0-jruby-1.7.8.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.9.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.9.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0-jruby-1.7.9.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.10.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.10.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0-jruby-1.7.10.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.8.7-jruby-1.7.11.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-1.9.3-jruby-1.7.11.tgz',
+      'https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/ruby-2.0.0-jruby-1.7.11.tgz',
+      'http://heroku-jdk.s3.amazonaws.com/openjdk1.7.0_25.tar.gz',
+      'http://heroku-jdk.s3.amazonaws.com/openjdk1.7.0_45.tar.gz',
+      'http://heroku-jdk.s3.amazonaws.com/openjdk7-latest.tar.gz'
+    ]
+  end
 
-  EXCLUDE_FROM_BUILDPACK = [
-    /\.git/,
-    /repos/
-  ]
-
-  class << self
-    def package(mode)
-      Dir.mktmpdir do |temp_dir|
-        copy_buildpack_contents(temp_dir)
-        download_dependencies(temp_dir) if mode == :offline
-        compress_buildpack(temp_dir)
-      end
-    end
-
-    private
-
-    def copy_buildpack_contents(target_path)
-      run_cmd "cp -r * #{target_path}"
-    end
-
-    def download_dependencies(target_path)
-      dependency_path = File.join(target_path, 'dependencies')
-
-      run_cmd "mkdir -p #{dependency_path}"
-
-      DEPENDENCIES.each do |uri|
-        run_cmd "cd #{dependency_path}; curl #{uri} -O"
-      end
-    end
-
-    def in_pack?(file)
-      !EXCLUDE_FROM_BUILDPACK.any? { |re| file =~ re }
-    end
-
-    def compress_buildpack(target_path)
-      Zip::File.open('ruby_buildpack.zip', Zip::File::CREATE) do |zipfile|
-        Dir[File.join(target_path, '**', '**')].each do |file|
-          zipfile.add(file.sub(target_path + '/', ''), file) if (in_pack?(file))
-        end
-      end
-    end
-
-    def run_cmd(cmd)
-      puts "$ #{cmd}"
-      `#{cmd}`
-    end
+  def excluded_files
+    [
+      /\.git/,
+      /repos/
+    ]
   end
 end
 
-BuildpackPackager.package(ARGV.first.to_sym)
+BuildpackPackager.new("ruby", ARGV.first.to_sym).package
