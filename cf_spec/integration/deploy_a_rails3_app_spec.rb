b832208ea9fb0790e3991a438fdbc7cc0fc0ed09
Jordi Noguera <jordi@pivotallabs.com>
2014-05-23 15:46:21 -0400

    Use Procfiles for test apps instead of having to run startup command

--- cf_spec/integration/deploy_a_rails3_app_spec.rb
+++ cf_spec/integration/deploy_a_rails3_app_spec.rb
@@ -3,10 +3,7 @@ require "cf_spec_helper"
 
 describe 'deploying a rails 3 application', :ruby_buildpack do
   it 'make the homepage available' do
-    Machete.deploy_app("rails3_mri_193", :ruby, {
-      cmd: "bundle exec rake db:migrate && bundle exec rails s -p $PORT",
-      with_pg: true
-    }) do |app|
+    Machete.deploy_app("rails3_mri_193", :ruby, with_pg: true) do |app|
       expect(app).to be_staged
       expect(app.homepage_html).to include('hello')
     end
--- cf_spec/integration/deploy_a_rails_latest_app_spec.rb
+++ cf_spec/integration/deploy_a_rails_latest_app_spec.rb
@@ -3,11 +3,7 @@ require "cf_spec_helper"
 
 describe 'deploying a rails 4 application', :ruby_buildpack do
   it 'make the homepage available' do
-    Machete.deploy_app("rails_latest_web_app", :ruby, {
-      cmd: "bundle exec rake db:migrate && bundle exec bin/rails s -p $PORT",
-      with_pg: true,
-      vendor_gems_before_push: true
-    }) do |app|
+    Machete.deploy_app("rails_latest_web_app", :ruby, with_pg: true) do |app|
       expect(app).to be_staged
       expect(app.homepage_html).to include('Listing people')
     end
