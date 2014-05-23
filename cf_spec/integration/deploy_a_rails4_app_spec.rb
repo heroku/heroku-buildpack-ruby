b9f5599ab6fef82f2c2bfd0b1c893d4512eb00ad
Jordi Noguera <jordi@pivotallabs.com>
2014-05-23 16:13:05 -0400

    test for rails apps pushed to online cf without vendored dependencies

--- cf_spec/integration/deploy_a_rails4_app_spec.rb
+++ cf_spec/integration/deploy_a_rails4_app_spec.rb
@@ -8,4 +8,14 @@ describe 'deploying a rails 4 application', :ruby_buildpack do
       expect(app.homepage_html).to include('The Kessel Run')
     end
   end
+
+  it "deploys apps without vendored dependencies", if: Machete::BuildpackMode.online? do
+    app_name = "rails4_web_app_no_dependencies"
+
+    Dir.exists?("cf_spec/fixtures/#{app_name}/vendor").should be_false
+    Machete.deploy_app(app_name, :ruby, with_pg: true) do |app|
+      expect(app).to be_staged
+      expect(app.homepage_html).to include('The Kessel Run')
+    end
+  end
 end
