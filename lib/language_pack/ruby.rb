f1d111fcef3823dedd4d09f79073ce9c1d70913a
schneems <richard.schneeman@gmail.com>
2014-06-16 12:58:44 -0500

    bundler 1.6.3

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -12,7 +12,7 @@ class LanguagePack::Ruby < LanguagePack::Base
   NAME                 = "ruby"
   LIBYAML_VERSION      = "0.1.6"
   LIBYAML_PATH         = "libyaml-#{LIBYAML_VERSION}"
-  BUNDLER_VERSION      = "1.5.2"
+  BUNDLER_VERSION      = "1.6.3"
   BUNDLER_GEM_PATH     = "bundler-#{BUNDLER_VERSION}"
   NODE_VERSION         = "0.4.7"
   NODE_JS_BINARY_PATH  = "node-#{NODE_VERSION}"
