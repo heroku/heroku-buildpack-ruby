zachlatta/heroku-buildpack-ruby-swagger

add stack change spec tests

spec/stack_spec.rb
@@ -1,0 +1,26 @@
+require_relative "spec_helper"
+
+describe "Stack Changes" do
+  fit "should reinstall gems on stack change" do
+    Hatchet::Runner.new("mri_210").deploy do |app, heroku|
+      heroku.put_stack(app.name, "cedar-14")
+      `git commit --allow-empty -m "cedar-14 migrate"`
+
+      app.push!
+      puts app.output
+      expect(app.output).to match("Installing rack 1.5.0")
+      expect(app.output).to match("Changing stack")
+    end
+  end
+
+  fit "should not reinstall gems if the stack did not change" do
+    Hatchet::Runner.new("mri_210").deploy do |app, heroku|
+      heroku.put_stack(app.name, "cedar")
+      `git commit --allow-empty -m "cedar migrate"`
+
+      app.push!
+      puts app.output
+      expect(app.output).to match("Using rack 1.5.0")
+    end
+  end
+end

heroku/heroku-buildpack-ruby

spec/stack_spec.rb
@@ -1,0 +1,26 @@
+require_relative "spec_helper"
+
+describe "Stack Changes" do
+  it "should reinstall gems on stack change" do
+    Hatchet::Runner.new("mri_200").deploy do |app, heroku|
+      heroku.put_stack(app.name, "cedar-14")
+      `git commit --allow-empty -m "cedar-14 migrate"`
+
+      app.push!
+      puts app.output
+      expect(app.output).to match("Installing rack 1.5.0")
+      expect(app.output).to match("Changing stack")
+    end
+  end
+
+  it "should not reinstall gems if the stack did not change" do
+    Hatchet::Runner.new("mri_200").deploy do |app, heroku|
+      heroku.put_stack(app.name, "cedar")
+      `git commit --allow-empty -m "cedar migrate"`
+
+      app.push!
+      puts app.output
+      expect(app.output).to match("Using rack 1.5.0")
+    end
+  end
+end