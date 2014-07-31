4c513f8e94b1233e42cb1aa619d97be82fe2a161
Terence Lee <hone02@gmail.com>
2014-07-31 16:06:56 -0700

    extract logic into NodeInstaller class, add cedar support back

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -5,6 +5,7 @@ require "rubygems"
 require "language_pack"
 require "language_pack/base"
 require "language_pack/ruby_version"
+require "language_pack/helpers/node_installer"
 require "language_pack/version"
 
 # base Ruby Language Pack. This is for any base ruby app.
@@ -14,9 +15,6 @@ class LanguagePack::Ruby < LanguagePack::Base
   LIBYAML_PATH         = "libyaml-#{LIBYAML_VERSION}"
   BUNDLER_VERSION      = "1.6.3"
   BUNDLER_GEM_PATH     = "bundler-#{BUNDLER_VERSION}"
-  NODE_VERSION         = "0.10.29"
-  NODE_JS_BINARY_PATH  = "node-v#{NODE_VERSION}-linux-x64"
-  NODEJS_BASE_URL      = "http://nodejs.org/dist/v#{NODE_VERSION}/"
   JVM_BASE_URL         = "http://heroku-jdk.s3.amazonaws.com"
   LATEST_JVM_VERSION   = "openjdk7-latest"
   LEGACY_JVM_VERSION   = "openjdk1.7.0_25"
@@ -42,10 +40,11 @@ class LanguagePack::Ruby < LanguagePack::Base
 
   def initialize(build_path, cache_path=nil)
     super(build_path, cache_path)
-    @fetchers[:mri] = LanguagePack::Fetcher.new(VENDOR_URL, @stack)
-    @fetchers[:jvm] = LanguagePack::Fetcher.new(JVM_BASE_URL)
-    @fetchers[:rbx] = LanguagePack::Fetcher.new(RBX_BASE_URL)
-    @fetchers[:nodejs] = LanguagePack::Fetcher.new(NODEJS_BASE_URL)
+    @fetchers[:mri]    = LanguagePack::Fetcher.new(VENDOR_URL, @stack)
+    @fetchers[:jvm]    = LanguagePack::Fetcher.new(JVM_BASE_URL)
+    @fetchers[:rbx]    = LanguagePack::Fetcher.new(RBX_BASE_URL)
+    @fetchers[:nodejs] = LanguagePack::Fetcher.new(LanguagePack::NodeInstaller::NODEJS_BASE_URL)
+    @node_installer    = LanguagePack::NodeInstaller.new(@fetchers, @stack)
   end
 
   def name
@@ -416,11 +415,8 @@ WARNING
     bin_dir = "bin"
     FileUtils.mkdir_p bin_dir
     Dir.chdir(bin_dir) do |dir|
-      if name == NODE_JS_BINARY_PATH
-        node_bin = "#{name}/bin/node"
-        @fetchers[:nodejs].fetch_untar("#{name}.tar.gz", "#{name}/bin/node")
-        FileUtils.mv(node_bin, ".")
-        FileUtils.rm_rf(name)
+      if name.match(/^node\-/)
+        @node_installer.install
       else
         @fetchers[:buildpack].fetch_untar("#{name}.tgz")
       end
@@ -696,7 +692,7 @@ params = CGI.parse(uri.query || "")
   # @note execjs will blow up if no JS RUNTIME is detected and is loaded.
   # @return [Array] the node.js binary path if we need it or an empty Array
   def add_node_js_binary
-    bundler.has_gem?('execjs') && !node_js_installed? ? [NODE_JS_BINARY_PATH] : []
+    bundler.has_gem?('execjs') && !node_js_installed? ? [@node_installer.binary_path] : []
   end
 
   def node_bp_bin_path
