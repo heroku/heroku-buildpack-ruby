6134e2c689d22eb6eeb4628b01c35682338d4602
Jacques Chester and Rick Reilly <pair+jchester+rreilly@pivotallabs.com>
2014-08-29 13:43:07 -0400

    Update CF fetcher to follow Heroku updates.
    Heroku change introduced in https://github.com/heroku/heroku-buildpack-ruby/commit/496f799e1465f7ed050015e4518507a4e47edc38#diff-d41d8cd98f00b204e9800998ecf8427e added a second parameter to fetch_untar. Our fetch_untar was incompatible with the change.
    
    [#77843004]

--- lib/cloud_foundry/language_pack/fetcher.rb
+++ lib/cloud_foundry/language_pack/fetcher.rb
@@ -7,8 +7,8 @@ if Dir.exist?(DEPENDENCIES_PATH)
         copy_file_from_dependencies_cache(path)
       end
 
-      def fetch_untar(path)
-        untar_file_from_dependencies_cache(path)
+      def fetch_untar(path, files_to_extract=nil)
+        untar_file_from_dependencies_cache(path, files_to_extract)
       end
 
       private
@@ -18,11 +18,11 @@ if Dir.exist?(DEPENDENCIES_PATH)
         run!("cp #{File.join(DEPENDENCIES_PATH, dependency_filename)} #{original_filename}")
       end
 
-      def untar_file_from_dependencies_cache(original_filename)
+      def untar_file_from_dependencies_cache(original_filename, files_to_extract="")
         original_filename = "#{original_filename}.mac" if ruby_200_on_OSX(original_filename)
 
         dependency_filename = Extensions.translate @host_url, original_filename
-        run!("tar zxf #{File.join(DEPENDENCIES_PATH, dependency_filename)}")
+        run!("tar zxf #{File.join(DEPENDENCIES_PATH, dependency_filename)} #{files_to_extract}")
       end
 
       def ruby_200_on_OSX(original_filename)
