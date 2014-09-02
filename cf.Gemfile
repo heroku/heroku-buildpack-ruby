fc2b2972d53d9d104e1cf94600cf663836679b69
Jacques Chester and Rick Reilly <pair+jchester+rreilly@pivotallabs.com>
2014-09-02 09:53:07 -0400

    Add rspec-retry

--- cf.Gemfile
+++ cf.Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 ruby '2.1.1'
 
 gem 'machete', github: 'cf-buildpacks/machete', branch: 'master'
+gem 'rspec-retry', '~> 0.3.0'
--- cf_spec/cf_spec_helper.rb
+++ cf_spec/cf_spec_helper.rb
@@ -1,6 +1,7 @@
 require 'bundler/setup'
 require 'machete'
 require 'machete/matchers'
+require 'rspec/retry'
 
 `mkdir -p log`
 Machete.logger = Machete::Logger.new("log/integration.log")
