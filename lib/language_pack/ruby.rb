270381f058f9ff2d5dd4c7a05f35ae64ead01b19
Joe Kutner <jpkutner@gmail.com>
2015-05-06 16:16:32 -0500

    Added override config var to build-time jruby-opts

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -247,9 +247,9 @@ EOF
 echo #{default_java_tool_options}
 SHELL
         if Gem::Version.new(ruby_version.engine_version) >= Gem::Version.new("1.7.12")
-          ENV["JRUBY_OPTS"] = "--dev"
+          ENV["JRUBY_OPTS"] = env('JRUBY_BUILD_OPTS') || "--dev"
         else
-          ENV["JRUBY_OPTS"] = "-Xcompile.invokedynamic=false -Xcompile.mode=OFF -J-XX:+TieredCompilation -J-XX:TieredStopAtLevel=1"
+          ENV["JRUBY_OPTS"] = env('JRUBY_BUILD_OPTS') || "-Xcompile.invokedynamic=false -Xcompile.mode=OFF -J-XX:+TieredCompilation -J-XX:TieredStopAtLevel=1"
         end
       end
       setup_ruby_install_env
