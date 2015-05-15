a9ffebd36387ecc0bf0860756c5950408f28f575
schneems <richard.schneeman@gmail.com>
2015-05-15 09:17:45 -0500

    add_database not required
    It was changed to be automatically provisioned.

--- spec/rails4_spec.rb
+++ spec/rails4_spec.rb
@@ -12,7 +12,6 @@ describe "Rails 4.0.x" do
 
   it "should deploy on ruby 2.0.0" do
     Hatchet::Runner.new("rails4-manifest").deploy do |app, heroku|
-      add_database(app, heroku)
       expect(app.output).to include("Detected manifest file, assuming assets were compiled locally")
       expect(app.output).not_to match("Include 'rails_12factor' gem to enable all platform features")
     end
@@ -28,7 +27,6 @@ describe "Rails 4.0.x" do
   it "upgraded from 3 to 4 missing ./bin still works" do
     Hatchet::Runner.new("rails3-to-4-no-bin").deploy do |app, heroku|
       expect(app.output).to include("Asset precompilation completed")
-      add_database(app, heroku)
 
       expect(app.output).to match("WARNING")
       expect(app.output).to match("Include 'rails_12factor' gem to enable all platform features")
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,14 +27,6 @@ def git_repo
   "https://github.com/heroku/heroku-buildpack-ruby.git"
 end
 
-def add_database(app, heroku)
-  Hatchet::RETRIES.times.retry do
-    heroku.post_addon(app.name, 'heroku-postgresql:hobby-dev')
-    _, value = heroku.get_config_vars(app.name).body.detect {|key, value| key.match(/HEROKU_POSTGRESQL_[A-Z]+_URL/) }
-    heroku.put_config_vars(app.name, 'DATABASE_URL' => value)
-  end
-end
-
 def successful_body(app, options = {})
   retry_limit = options[:retry_limit] || 50
   Excon.get("http://#{app.name}.herokuapp.com", :idempotent => true, :expects => 200, :retry_limit => retry_limit).body
