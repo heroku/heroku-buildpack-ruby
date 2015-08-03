61b910f793f9cdc259977f649f97aed02651a8c7
schneems <richard.schneeman@gmail.com>
2015-08-03 14:18:03 -0500

    Fix status 422
    The DATABASE_URL was already being set correctly on some apps so when we tried to provision and update the DATABASE_URL we were getting a 422 http://stackoverflow.com/questions/30611801/how-to-change-database-url-on-heroku
    
    Pretty sure we don't need this

--- spec/rails41_spec.rb
+++ spec/rails41_spec.rb
@@ -12,7 +12,6 @@ describe "Rails 4.1.x" do
 
   it "should be able to run a migration without heroku specific database.yml" do
     Hatchet::Runner.new("rails41_scaffold").deploy do |app, heroku|
-      add_database(app, heroku)
       expect(app.output).not_to include("Writing config/database.yml to read from DATABASE_URL")
       expect(app.run("rake db:migrate")).to include("20140218165801 CreatePeople")
     end
@@ -20,7 +19,6 @@ describe "Rails 4.1.x" do
 
   it "should handle secrets.yml properly" do
     Hatchet::Runner.new("rails41_scaffold").deploy do |app, heroku|
-      add_database(app, heroku)
       ReplRunner.new(:rails_console, "heroku run bin/rails console -a #{app.name}").run do |console|
         console.run("ENV['SECRET_KEY_BASE'] == Rails.application.config.secrets.secret_key_base") {|result| expect(result).not_to eq("true") }
       end
