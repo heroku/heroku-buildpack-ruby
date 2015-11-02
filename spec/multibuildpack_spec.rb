70523986aa986ed4b393cafd0fa9f4bb2effbd7f
schneems <richard.schneeman@gmail.com>
2015-11-02 13:01:05 -0600

    update spec

--- spec/multibuildpack_spec.rb
+++ spec/multibuildpack_spec.rb
@@ -5,6 +5,7 @@ describe "Multibuildpack" do
     Hatchet::Runner.new("node_multi", buildpack_url: "https://github.com/ddollar/heroku-buildpack-multi.git").deploy do |app|
       puts app.output
       expect(app.output).to match("Node Version in Ruby buildpack is: v4.1.2")
+      expect(app.run("node -v")).to match("v4.1.2")
     end
   end
 end
