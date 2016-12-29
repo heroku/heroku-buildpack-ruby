52d3bc749211ce45df2b1f9d4127cb57a7b28263
puneet <puneet@bigbinary.com>
2016-12-29 18:59:41 +0530

    Adding webpacker compile task

--- lib/language_pack/rails51.rb
+++ lib/language_pack/rails51.rb
@@ -29,6 +29,7 @@ class LanguagePack::Rails51 < LanguagePack::Rails5
       allow_git do
         puts "installing yarn"
         puts @yarn_installer.install
+        puts @node_installer.install
         puts "installing yarn done"
         install_node_packages
         run_webpack_compile_rake_task
