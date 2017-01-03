8a13cf8e7762ac52993ac32e5f06bed54a7328f2
Terence Lee <hone02@gmail.com>
2017-01-03 13:48:40 -0600

    switch from ddollar multi to heroku multi

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Multibuildpack" do
   it "works with node" do
-    Hatchet::Runner.new("node_multi", buildpack_url: "https://github.com/ddollar/heroku-buildpack-multi.git").deploy do |app|
+    Hatchet::Runner.new("node_multi", buildpack_url: "https://github.com/heroku/heroku-buildpack-multi.git").deploy do |app|
       expect(app.output).to match("Node Version in Ruby buildpack is: v4.1.2")
       expect(app.run("node -v")).to match("v4.1.2")
     end
