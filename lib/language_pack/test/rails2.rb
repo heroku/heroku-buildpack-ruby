31dc4db3605b8c6e54b6d8e3441a70a652bd0a18
Terence Lee <hone02@gmail.com>
2017-01-11 18:02:28 -0600

    blank out db:test:purge for rails

--- lib/language_pack/test/rails2.rb
+++ lib/language_pack/test/rails2.rb
@@ -17,4 +17,25 @@ class LanguagePack::Rails2
   def rake_env
     super.merge(default_env_vars)
   end
+
+  def prepare_tests
+    super
+    topic "Clearing db:test:purge rake task"
+    blank_db_test_purge_task
+  end
+
+  # rails test runner + rspec depend on db:test:purge which drops/creates a db which doesn't work on Heroku's DB plans
+  def blank_db_test_purge_task
+    File.open("lib/tasks/heroku_db_test_purge.rake", "w") do |file|
+      file.puts <<FILE
+Rake::Task["db:test:purge"].clear
+namespace :db do
+  namespace :test do
+    task :purge do
+    end
+  end
+end
+FILE
+    end
+  end
 end
