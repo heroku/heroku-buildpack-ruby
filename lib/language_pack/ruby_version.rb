de82a91007474bacbf9247bc28135c28de51b110
Joe Kutner <jpkutner@gmail.com>
2017-02-22 10:57:32 -0500

    Add jruby to patchlevel_is_significant? test

--- lib/language_pack/ruby_version.rb
+++ lib/language_pack/ruby_version.rb
@@ -52,7 +52,7 @@ module LanguagePack
     # Before Ruby 2.1 patch releases were done via patchlevel i.e. 1.9.3-p426 versus 1.9.3-p448
     # With 2.1 and above patches are released in the "minor" version instead i.e. 2.1.0 versus 2.1.1
     def patchlevel_is_significant?
-      Gem::Version.new(self.ruby_version) <= Gem::Version.new("2.1")
+      !jruby? && Gem::Version.new(self.ruby_version) <= Gem::Version.new("2.1")
     end
 
     def rake_is_vendored?
