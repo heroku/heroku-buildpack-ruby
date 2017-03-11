4c6a582e4eae04516515930ded5b2cba11da1f22
Terence Lee <hone02@gmail.com>
2017-03-10 18:28:18 -0600

    update yarn test

--- spec/hatchet/rails51_spec.rb
+++ spec/hatchet/rails51_spec.rb
@@ -1,9 +1,10 @@
 require "spec_helper"
 
 describe "Rails 5.1" do
-  fit "works with webpacker + yarn (js friends)" do
+  it "works with webpacker + yarn (js friends)" do
     Hatchet::Runner.new("rails51_webpacker").deploy do |app, heroku|
-      puts app.output
+      expect(app.output).to include("Installing yarn")
+      expect(app.output).to include("yarn install")
       expect(app.run("rails -v")).to match("")
     end
   end
