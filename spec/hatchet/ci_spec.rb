0802d50b48cd94d15f01983d3a640495337d1fe4
schneems <richard.schneeman@gmail.com>
2017-03-30 14:40:52 -0500

    Test CI with Hatchet 2.0

--- spec/hatchet/ci_spec.rb
+++ spec/hatchet/ci_spec.rb
@@ -6,4 +6,28 @@ describe "CI" do
       expect(test_run.status).to eq(:succeeded)
     end
   end
+
+  it "Works with Rails 5 ruby schema apps" do
+    Hatchet::Runner.new("rails5_ruby_schema_format").run_ci do |test_run|
+      expect(test_run.output).to match("db:schema:load_if_ruby completed")
+    end
+  end
+
+  it "Works with Rails 5 SQL schema apps" do
+    Hatchet::Runner.new("rails5_sql_schema_format").run_ci do |test_run|
+      expect(test_run.output).to match("db:structure:load_if_sql completed")
+    end
+  end
+
+  it "Works with Rails 3.1 ruby schema apps" do
+    Hatchet::Runner.new("rails_31_ruby_schema_format").run_ci do |test_run|
+      expect(test_run.output).to match("db:schema:load completed")
+    end
+  end
+
+  it "Works with Rails 3.1 SQL schema apps" do
+    Hatchet::Runner.new("rails_31_sql_schema_format").run_ci do |test_run|
+      expect(test_run.output).to match("db:structure:load completed")
+    end
+  end
 end
