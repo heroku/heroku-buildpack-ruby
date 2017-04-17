d48c8e5a05458847aed48c461250a910ac832c0c
Soulou <leo@unbekandt.eu>
2017-04-17 23:45:16 +0200

    remove macos-only code

--- lib/cloud_foundry/language_pack/fetcher.rb
+++ lib/cloud_foundry/language_pack/fetcher.rb
@@ -19,14 +19,8 @@ if Dir.exist?(DEPENDENCIES_PATH)
       end
 
       def untar_file_from_dependencies_cache(original_filename, files_to_extract="")
-        original_filename = "#{original_filename}.mac" if ruby_200_on_OSX(original_filename)
-
         dependency_filename = Extensions.translate @host_url, original_filename
         run!("tar zxf #{File.join(DEPENDENCIES_PATH, dependency_filename)} #{files_to_extract}")
-      end
-
-      def ruby_200_on_OSX(original_filename)
-        original_filename.match('ruby-2.0.0') && `uname -s`.match('Darwin')
       end
     end
   end
