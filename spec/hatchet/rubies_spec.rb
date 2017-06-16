dbe27c7e3b1467b4fd5eaa3e01c182755035731d
schneems <richard.schneeman@gmail.com>
2017-06-16 16:49:24 -0500

    Remove heroku API calls

--- spec/hatchet/rubies_spec.rb
+++ spec/hatchet/rubies_spec.rb
@@ -2,9 +2,8 @@ require_relative '../spec_helper'
 
 describe "Ruby Versions on cedar-14" do
   it "should allow patchlevels" do
-    app = Hatchet::Runner.new('mri_193_p547')
+    app = Hatchet::Runner.new('mri_193_p547', stack: "cedar-14")
     app.setup!
-    app.heroku.put_stack(app.name, "cedar-14")
     app.deploy do |app|
       version = '1.9.3p547'
       expect(app.output).to match("ruby-1.9.3-p547")
@@ -13,9 +12,8 @@ describe "Ruby Versions on cedar-14" do
   end
 
   it "should deploy ruby 1.9.2 properly" do
-    app = Hatchet::Runner.new('mri_192')
+    app = Hatchet::Runner.new('mri_192', stack: "cedar-14")
     app.setup!
-    app.heroku.put_stack(app.name, "cedar-14")
     app.deploy do |app|
       version = '1.9.2'
       expect(app.output).to match(version)
@@ -24,9 +22,8 @@ describe "Ruby Versions on cedar-14" do
   end
 
   it "should deploy ruby 1.9.3 properly" do
-    app = Hatchet::Runner.new('mri_193')
+    app = Hatchet::Runner.new('mri_193', stack: "cedar-14")
     app.setup!
-    app.heroku.put_stack(app.name, "cedar-14")
     app.deploy do |app|
       version = '1.9.3'
       expect(app.output).to match(version)
@@ -35,9 +32,8 @@ describe "Ruby Versions on cedar-14" do
   end
 
   it "should deploy ruby 2.0.0 properly" do
-    app = Hatchet::Runner.new('mri_200')
+    app = Hatchet::Runner.new('mri_200', stack: "cedar-14")
     app.setup!
-    app.heroku.put_stack(app.name, "cedar-14")
     app.deploy do |app|
       version = '2.0.0'
       expect(app.output).to match(version)
@@ -48,9 +44,8 @@ describe "Ruby Versions on cedar-14" do
   end
 
   it "should deploy jdk 8 on cedar-14 by default" do
-    app = Hatchet::Runner.new("ruby_193_jruby_17161")
+    app = Hatchet::Runner.new("ruby_193_jruby_17161", stack: "cedar-14")
     app.setup!
-    app.heroku.put_stack(app.name, 'cedar-14')
     app.deploy do |app|
       expect(app.output).to match("Installing JVM: openjdk-8")
       expect(app.output).to match("JRUBY_OPTS is:  -Xcompile.invokedynamic=false")
@@ -66,9 +61,8 @@ describe "Ruby Versions on cedar-14" do
   end
 
   it "should deploy jruby 1.7.16.1 (jdk 7) properly on cedar-14 with sys props file" do
-    app = Hatchet::Runner.new("ruby_193_jruby_17161_jdk7")
+    app = Hatchet::Runner.new("ruby_193_jruby_17161_jdk7", stack: "cedar-14")
     app.setup!
-    app.heroku.put_stack(app.name, 'cedar-14')
     app.deploy do |app|
       expect(app.output).to match("Installing JVM: openjdk-7")
       expect(app.output).not_to include("OpenJDK 64-Bit Server VM warning")
