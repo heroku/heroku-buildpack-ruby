d10664bb248b52c009c25adbb3b461580f89f25a
schneems <richard.schneeman@gmail.com>
2017-11-02 09:51:01 -0500

    Speed up deploys, don’t check latest bundler version.
    http://bundler.io/man/bundle-config.1.html
    
    ```
    disable_version_check (BUNDLE_DISABLE_VERSION_CHECK): Stop Bundler from checking if a newer Bundler version is available on rubygems.org.
    ```

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -611,7 +611,8 @@ WARNING
             "LIBRARY_PATH"                  => noshellescape("#{yaml_lib}:$LIBRARY_PATH"),
             "RUBYOPT"                       => syck_hack,
             "NOKOGIRI_USE_SYSTEM_LIBRARIES" => "true",
-            "JAVA_HOME"                     => noshellescape("#{pwd}/$JAVA_HOME")
+            "JAVA_HOME"                     => noshellescape("#{pwd}/$JAVA_HOME"),
+            "BUNDLE_DISABLE_VERSION_CHECK"  => "true"
           }
           env_vars["BUNDLER_LIB_PATH"] = "#{pwd}/#{bundler_path}/lib" if ruby_version.ruby_version == "1.8.7"
           puts "Running: #{bundle_command}"
