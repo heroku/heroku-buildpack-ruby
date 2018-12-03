64debcb03680b196fd42ae265acb5a4178a9869e
Jeremy Morrell <jmorrell@heroku.com>
2018-12-03 10:09:56 -0800

    Update included Node to latest LTS version

--- lib/language_pack/helpers/nodebin.rb
+++ lib/language_pack/helpers/nodebin.rb
@@ -11,7 +11,7 @@ class LanguagePack::Helpers::Nodebin
   end
 
   def self.hardcoded_node_lts
-    version = "8.10.0"
+    version = "10.14.1"
     {
       "number" => version,
       "url"    => "https://s3.amazonaws.com/heroku-nodejs-bins/node/release/linux-x64/node-v#{version}-linux-x64.tar.gz"
