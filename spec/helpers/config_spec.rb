09b70540f1e046ac47577f8acdc708d844658854
schneems <richard.schneeman+foo@gmail.com>
2019-05-28 14:21:08 -0500

    Test that the ruby version string is also present in ruby vendor strings

--- spec/helpers/config_spec.rb
+++ spec/helpers/config_spec.rb
@@ -6,5 +6,10 @@ describe "Boot Strap Config" do
     config = TomlRB.load_file("buildpack.toml")
     bootstrap_version = config["buildpack"]["ruby_version"]
     expect(bootstrap_version).to eq(LanguagePack::RubyVersion::DEFAULT_VERSION_NUMBER)
+
+    urls = config["publish"]["Vendor"].map {|h| h["url"] if h["dir"] != "." }.compact
+    urls.each do |url|
+      expect(url.include?(bootstrap_version)).to be_truthy, "expected #{url.inspect} to include #{bootstrap_version.inspect} but it did not"
+    end
   end
 end
