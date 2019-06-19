f0349079ae8efec15e673d15ceaaed249210daa0
schneems <richard.schneeman+foo@gmail.com>
2019-06-19 12:47:18 -0500

    Bump buildpack ruby version to 2.6.3
    Due to bundler/bundler#7205. When the buildpack tries to detect the version of Ruby used by the app it gets an error if they are using bundler 2.0.2 and bundler on the system is 2.0.1:
    
    ```
    remote: !
    remote: ! There was an error parsing your Gemfile, we cannot continue
    remote: ! /app/tmp/buildpacks/b7af5642714be4eddaa5f35e2b4c36176b839b4abcd9bfe57ee71c358d71152b4fd2cf925c5b6e6816adee359c4f0f966b663a7f8649b0729509d510091abc07/vendor/ruby/heroku-18/lib/ruby/2.5.0/rubygems.rb:289:in find_spec_for_exe': can't find gem bundler (>= 0.a) with executable bundle (Gem::GemNotFoundException) remote: ! from /app/tmp/buildpacks/b7af5642714be4eddaa5f35e2b4c36176b839b4abcd9bfe57ee71c358d71152b4fd2cf925c5b6e6816adee359c4f0f966b663a7f8649b0729509d510091abc07/vendor/ruby/heroku-18/lib/ruby/2.5.0/rubygems.rb:308:inactivate_bin_path'
    remote: ! from /tmp/d20190618-115-la0wgl/bundler-2.0.1/bin/bundle:23:in `'
    remote: !
    ```
    
    Ruby 2.6.3 has a more recent version of RubyGems that should not trigger this failing logic.

--- spec/hatchet/bundler_spec.rb
+++ spec/hatchet/bundler_spec.rb
@@ -8,4 +8,15 @@ describe "Bundler" do
       expect(app.output).to match("Installing dependencies using bundler 2.")
     end
   end
+
+  it "deploys with version 2.0.2 and Ruby 2.5.5" do
+    before_deploy = -> {
+      run!(%Q{printf "ruby '2.5.5'" >> Gemfile})
+      run!(%Q{printf "\nBUNDLED WITH\n   2.0.2\n" >> Gemfile.lock})
+    }
+
+    Hatchet::Runner.new("default_ruby", before_deploy: before_deploy).deploy do |app|
+      expect(app.output).to match("Installing dependencies using bundler 2.")
+    end
+  end
 end
