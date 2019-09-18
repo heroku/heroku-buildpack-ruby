23ed181e9e07de6fb5357ccba18ae0b251d7107e
Kevin Pheasey <kevin@kpsoftware.io>
2019-09-18 08:47:46 -0400

    fix bad merge and use var to avoid run on array line

--- lib/language_pack.rb
+++ lib/language_pack.rb
@@ -13,7 +13,21 @@ module LanguagePack
     Instrument.instrument 'detect' do
       Dir.chdir(args.first)
 
-      pack = [ NoLockfile, Rails6, Rails5, Rails42, Rails41, Rails4, Rails3, Rails2, Rack, Ruby ].detect do |klass|
+      packs = [
+        NoLockfile,
+        Rails6,
+        Rails51,
+        Rails5,
+        Rails42,
+        Rails41,
+        Rails4,
+        Rails3,
+        Rails2,
+        Rack,
+        Ruby
+      ]
+
+      pack = packs.detect do |klass|
         klass.use?
       end
 
