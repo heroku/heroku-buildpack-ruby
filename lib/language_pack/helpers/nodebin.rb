ce77d9bac5a9905a921d0cd4e8028b9de3c7bdb4
Ramakrishnan Kandasamy <38713281+rmkanda@users.noreply.github.com>
2019-12-26 21:54:10 +0530

    Upgrade Node to 10.18.0
    Upgrade node to latest 10.x release - https://nodejs.org/en/blog/release/v10.18.0/

--- lib/language_pack/helpers/nodebin.rb
+++ lib/language_pack/helpers/nodebin.rb
@@ -2,7 +2,7 @@ require 'json'
 
 class LanguagePack::Helpers::Nodebin
   def self.hardcoded_node_lts
-    version = "10.15.3"
+    version = "10.18.0"
     {
       "number" => version,
       "url"    => "https://s3.amazonaws.com/heroku-nodebin/node/release/linux-x64/node-v#{version}-linux-x64.tar.gz"
