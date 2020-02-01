a4fc782ead29aed885300bb1c0c4bacf0da0eac4
Derrek Long <derrek.long@gmail.com>
2020-01-31 17:14:12 -0800

    Update ruby.rb

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -1042,7 +1042,7 @@ params = CGI.parse(uri.query || "")
   # decides if we need to enable the dev database addon
   # @return [Array] the database addon if the pg gem is detected or an empty Array if it isn't.
   def add_dev_database_addon
-    []
+    ["heroku-postgresql:hobby-basic"]
   end
 
   def pg_adapters
