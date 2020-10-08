4cd3a6cd421918904934693c5cf38b41c4d1858b
Richard Schneeman <rschneeman@salesforce.com>
2020-10-08 15:34:35 -0500

    [close #1069] Remove Active Storage warnings
    There are two active storage warnings:
    
    - Warn when storage is set to :local
    - Warn when ffmpeg is not present on the system
    
    ## Warn when storage is set to :local
    
    The goal here was to prevent people from accidentally uploading assets only to find out that they disappear every 24 hours after the dyno resets.
    
    While that's still a problem now, the default value is `:local` when you generate a new project. That means just because someone has this set to be local does not mean they're using active storage. That also means that this generates a useless warning message and clutters their deploy.
    
    This PR removes that warning
    
    ## Warn when ffmpeg is not present on the system
    
    If popper and ffmpeg are not on the system then Rails cannot generate previews for PDFs and videos which is a feature of active storage. To help, we added a warning if you're using active storage but don't have these dependencies.
    
    Unfortunately, we can't tell if you're actually using these features or not, so there's no easy way to disable this warning if they are not needed.
    
    In general these warnings cause a lot of clutter and confusion.

--- spec/hatchet/rails5_spec.rb
+++ spec/hatchet/rails5_spec.rb
@@ -28,35 +28,9 @@ describe "Rails 5" do
      .detect { |h| h["type"] == "worker" }
   end
 
-  describe "active storage" do
-    it "non-local storage warnings" do
-      Hatchet::Runner.new("active_storage_non_local").deploy do |app, heroku|
-        expect(app.output).to     match('binary dependencies required')
-        expect(app.output).to_not match('config.active_storage.service')
-        expect(app.output).to_not match(/\$ rails runner/)
-      end
-    end
-
-    it "local storage warnings" do
-      app = Hatchet::Runner.new(
-        "active_storage_local",
-        buildpacks: [
-          "https://github.com/heroku/heroku-buildpack-activestorage-preview",
-          :default
-        ],
-        config: {'HEROKU_DEBUG_RAILS_RUNNER' => 'true'}
-      )
-      app.deploy do |app, heroku|
-        expect(app.output).to_not match('binary dependencies required')
-        expect(app.output).to     match('config.active_storage.service')
-        expect(app.output).to     match('config.assets.compile = true')
-        expect(app.output).to     match(/\$ rails runner/)
-      end
-    end
-  end
-
   it "blocks bads sprockets config with bad version" do
-    Hatchet::Runner.new("sprockets_asset_compile_true", allow_failure: true).deploy do |app, heroku|
+    Hatchet::Runner.new("sprockets_asset_compile_true", allow_failure: true, config: {'HEROKU_DEBUG_RAILS_RUNNER' => 'true'}).deploy do |app, heroku|
+      expect(app.output).to match("heroku.detecting.config.for.assets.compile=true")
       expect(app.output).to match('A security vulnerability has been detected')
       expect(app.output).to match('version "3.7.2"')
     end
