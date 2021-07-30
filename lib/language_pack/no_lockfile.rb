32318ab02029266999ba357f73e65f5e0d00a29b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-07-31 10:58:35 +1200

    Bypass `NoLockfile` when `gems.locked` is present.

--- lib/language_pack/no_lockfile.rb
+++ lib/language_pack/no_lockfile.rb
@@ -3,7 +3,8 @@ require "language_pack/ruby"
 
 class LanguagePack::NoLockfile < LanguagePack::Ruby
   def self.use?
-    !File.exists?("Gemfile.lock")
+    !File.exist?("Gemfile.lock") &&
+    !File.exist?("gems.locked")
   end
 
   def name
@@ -11,6 +12,6 @@ class LanguagePack::NoLockfile < LanguagePack::Ruby
   end
 
   def compile
-    error "Gemfile.lock required. Please check it in."
+    error "'Gemfile.lock' or 'gems.locked' required. Please check it in."
   end
 end
