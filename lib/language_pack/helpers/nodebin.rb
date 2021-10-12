578082be8b6e692df4cbae0b3fc6dc8b56171eca
Morgan Grubb <morgan.grubb@gmail.com>
2021-10-12 13:20:09 -0700

    Bump node and yarn versions

--- lib/language_pack/helpers/nodebin.rb
+++ lib/language_pack/helpers/nodebin.rb
@@ -2,7 +2,7 @@ require 'json'
 
 class LanguagePack::Helpers::Nodebin
   def self.hardcoded_node_lts
-    version = "12.16.2"
+    version = "16.10.0"
     {
       "number" => version,
       "url"    => "https://s3.amazonaws.com/heroku-nodebin/node/release/linux-x64/node-v#{version}-linux-x64.tar.gz"
@@ -10,7 +10,7 @@ class LanguagePack::Helpers::Nodebin
   end
 
   def self.hardcoded_yarn
-    version = "1.22.4"
+    version = "1.22.15"
     {
       "number" => version,
       "url"    => "https://s3.amazonaws.com/heroku-nodebin/yarn/release/yarn-v#{version}.tar.gz"
