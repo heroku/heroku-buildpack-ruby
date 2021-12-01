a7d9d8938eb3d7d199e15128bc447aaec75fb526
Richard Schneeman <rschneeman@salesforce.com>
2021-12-01 15:24:17 -0600

    Fix Rails 7 tests
    This test started failing because the `turbo-rails` gem we were using was yanked. https://github.com/hotwired/turbo-rails/issues/273. This commit revs the hatchet lock version to point at the latest changes to the fixture: https://github.com/sharpstone/rails-jsbundling/commit/192a71e0931aa00724412521dcad4d1e1db2ec0f
    
    GUS-W-10261468

--- spec/hatchet/rails7_spec.rb
+++ spec/hatchet/rails7_spec.rb
@@ -9,8 +9,6 @@ describe "Rails 6" do
   end
 
   it "works with jsbundling" do
-    skip("rails-turbo was yanked and I don't know why")
-
     Hatchet::Runner.new("rails-jsbundling").tap do |app|
       app.deploy do
         expect(app.output).to include("yarn install")
