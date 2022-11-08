bd8666a852d0eec06f4defc548679b798800bfb2
Richard Schneeman <rschneeman@salesforce.com>
2022-11-08 12:54:58 -0600

    Remove circle tests (#1326)
    * Remove circle tests
    
    The CNB codebase is now at https://github.com/heroku/buildpacks-ruby and we're moving away from CircleCI
    
    GUS-W-11832774
    
    * Remove unused tests
    
    * Fix failing tests on heroku-22
    
    Rails LTS does not yet support Ruby 3.1 so these tests cannot run on Heroku-22 which is now the default.
    
    * Remove cutlass dep

--- spec/hatchet/rails6_spec.rb
+++ spec/hatchet/rails6_spec.rb
@@ -2,35 +2,9 @@ require_relative '../spec_helper'
 
 describe "Rails 6" do
   it "should detect successfully" do
-    Hatchet::App.new('rails6-basic').in_directory_fork do
+    Hatchet::App.new('rails61').in_directory_fork do
       expect(LanguagePack::Rails5.use?).to eq(false)
       expect(LanguagePack::Rails6.use?).to eq(true)
-    end
-  end
-
-  it "calls bin/yarn no matter what is on the path" do
-    Hatchet::Runner.new("rails6-basic").tap do |app|
-      # We put our version of yarn first on the path ahead of bin/yarn
-      # however webpacker explicitly calls bin/yarn instead of calling
-      # `yarn install`
-      app.before_deploy do
-        File.open("bin/yarn", "w") do |f|
-          f.write <<~EOM
-          #! /usr/bin/env bash
-
-          echo "Called bin/yarn binstub"
-          `yarn install`
-          EOM
-        end
-        run("chmod +x bin/yarn")
-      end
-
-      app.deploy do
-        expect(app.output).to include("Called bin/yarn binstub")
-
-        expect(app.output).to match("rake assets:precompile")
-        expect(app.output).to match("rake assets:clean")
-      end
     end
   end
 
@@ -43,7 +17,7 @@ describe "Rails 6" do
       run! %Q{echo 'task "assets:precompile" do ; end' > Rakefile}
     end
 
-    Hatchet::Runner.new('rails6-basic', before_deploy: before_deploy).deploy do |app|
+    Hatchet::Runner.new('rails61', before_deploy: before_deploy).deploy do |app|
       expect(app.output).to match("Fetching railties 6")
 
       expect(app.output).to match("rake assets:precompile")
