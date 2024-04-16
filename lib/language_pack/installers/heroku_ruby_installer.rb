5f9fbd1a428a87a7da00550e0ac139f04dc9c451
Richard Schneeman <rschneeman@salesforce.com>
2024-04-16 10:08:59 -0500

    Update lib/language_pack/installers/heroku_ruby_installer.rb
    Co-authored-by: Josh W Lewis <josh.w.lewis@gmail.com>

--- lib/language_pack/installers/heroku_ruby_installer.rb
+++ lib/language_pack/installers/heroku_ruby_installer.rb
@@ -11,7 +11,7 @@ class LanguagePack::Installers::HerokuRubyInstaller
   attr_reader :fetcher
 
   def initialize(stack: )
-      @fetcher = LanguagePack::Fetcher.new(BASE_URL, stack: stack)
+    @fetcher = LanguagePack::Fetcher.new(BASE_URL, stack: stack)
   end
 
   def install(ruby_version, install_dir)
