ed9a3bc7d4fa566f879f8e4d5ab8c4f82b2cc1a9
Agung Yuliaji <orang@ganteng.id>
2024-05-29 16:54:26 +0700

    Update nodebin.rb to support CUSTOM_HEROKU_NODEBIN_HOST env

--- lib/language_pack/helpers/nodebin.rb
+++ lib/language_pack/helpers/nodebin.rb
@@ -3,18 +3,19 @@ require 'json'
 class LanguagePack::Helpers::Nodebin
   NODE_VERSION = "20.9.0"
   YARN_VERSION = "1.22.19"
-
+  CUSTOM_HEROKU_NODEBIN_HOST = ENV["CUSTOM_HEROKU_NODEBIN_HOST"] || "https://heroku-nodebin.s3.us-east-1.amazonaws.com"
+  
   def self.hardcoded_node_lts
     {
       "number" => NODE_VERSION,
-      "url"    => "https://heroku-nodebin.s3.us-east-1.amazonaws.com/node/release/linux-x64/node-v#{NODE_VERSION}-linux-x64.tar.gz"
+      "url"    => "#{CUSTOM_HEROKU_NODEBIN_HOST}/node/release/linux-x64/node-v#{NODE_VERSION}-linux-x64.tar.gz"
     }
   end
 
   def self.hardcoded_yarn
     {
       "number" => YARN_VERSION,
-      "url"    => "https://heroku-nodebin.s3.us-east-1.amazonaws.com/yarn/release/yarn-v#{YARN_VERSION}.tar.gz"
+      "url"    => "#{CUSTOM_HEROKU_NODEBIN_HOST}/yarn/release/yarn-v#{YARN_VERSION}.tar.gz"
     }
   end
 
