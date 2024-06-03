b467a317a0eeb0c23e6b35cc99d08be1d09918f9
Schneems <richard.schneeman+foo@gmail.com>
2024-06-03 11:44:55 -0500

    Update latest outdated Ruby version test.

--- spec/helpers/outdated_ruby_version_spec.rb
+++ spec/helpers/outdated_ruby_version_spec.rb
@@ -19,7 +19,7 @@ describe LanguagePack::Helpers::OutdatedRubyVersion do
     )
 
     outdated.call
-    expect(outdated.suggested_ruby_minor_version).to eq("3.1.5")
+    expect(outdated.suggested_ruby_minor_version).to eq("3.1.6")
   end
 
   it "handles arm 💪 architecture on heroku-24" do
@@ -35,7 +35,7 @@ describe LanguagePack::Helpers::OutdatedRubyVersion do
     )
 
     outdated.call
-    expect(outdated.suggested_ruby_minor_version).to eq("3.1.5")
+    expect(outdated.suggested_ruby_minor_version).to eq("3.1.6")
   end
 
   it "finds the latest version on a stack" do
