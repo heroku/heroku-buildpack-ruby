1cd375ad16b06ee938068593d29ecf4afca52ce3
Stella Yang <stella@stellayang.dev>
2024-06-13 17:44:35 -0700

    add a test to check that this runs on heroku-24
    expect new line at the end of the `which` command output by stripping it to satisfy the test
    
    using the getting started fixture as expected by the current spec file
    
    Co-Authored-By: Richard Schneeman <rschneeman@salesforce.com>

--- spec/hatchet/getting_started_spec.rb
+++ spec/hatchet/getting_started_spec.rb
@@ -6,4 +6,10 @@ describe "Heroku ruby getting started" do
       expect(app.run("ls tmp/cache/assets")).to_not match("sprockets")
     end
   end
+
+  it "works on Heroku-24" do
+    Hatchet::Runner.new("ruby-getting-started", stack: "heroku-24").deploy do |app|
+      expect(app.run("which ruby").strip).to eq("/app/bin/ruby")
+    end
+  end
 end
