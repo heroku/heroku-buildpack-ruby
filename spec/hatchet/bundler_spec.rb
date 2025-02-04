d415d2a5e4f735b5e09bda1d5a1f0a9355121f36
Schneems <richard.schneeman+foo@gmail.com>
2025-02-04 13:35:46 -0600

    Remove bundler 1.x spec
    This no longer builds on the platform:
    
    ```
           remote:  !     There was an error parsing your Gemfile, we cannot continue        
           remote:  !     /tmp/d20250204-129-9ld2nw/bundler-1.17.3/gems/bundler-1.17.3/lib/bundler/shared_helpers.rb:29:in `root': undefined method `untaint' for an instance of Pathname (NoMethodError)        
           remote:  !             
           remote:  !     Pathname.new(gemfile).untaint.expand_path.parent        
           remote:  !     ^^^^^^^^        
           remote:  !     from /tmp/d20250204-129-9ld2nw/bundler-1.17.3/gems/bundler-1.17.3/lib/bundler.rb:234:in `root'        
           remote:  !     from /tmp/d20250204-129-9ld2nw/bundler-1.17.3/gems/bundler-1.17.3/lib/bundler.rb:246:in `app_config_path'       
    ```

--- spec/hatchet/bundler_spec.rb
+++ spec/hatchet/bundler_spec.rb
@@ -9,29 +9,4 @@ describe "Bundler" do
       end
     end
   end
-
-  it "deploys with version 1.x" do
-    abi_version = LanguagePack::RubyVersion::DEFAULT_VERSION_NUMBER.dup
-    abi_version[-1] = "0" # turn 2.6.6 into 2.6.0
-    pending("Must enable HATCHET_EXPENSIVE_MODE") unless ENV["HATCHET_EXPENSIVE_MODE"]
-
-    Hatchet::Runner.new("default_ruby").tap do |app|
-      app.before_deploy do
-        run!(%Q{printf "\nBUNDLED WITH\n   1.0.1\n" >> Gemfile.lock})
-      end
-      app.deploy do
-        expect(app.output).to match("Installing dependencies using bundler 1.")
-        expect(app.output).to match("BUNDLE_GLOBAL_PATH_APPENDS_RUBY_SCOPE=1")
-
-        # app.run_multi("ls vendor/bundle/ruby/#{abi_version}/gems") do |ls_output|
-        #   expect(ls_output).to match("rake-")
-        # end
-
-        app.run("which -a rake") do |which_rake|
-          expect(which_rake).to include("/app/vendor/bundle/bin/rake")
-          expect(which_rake).to include("/app/vendor/bundle/ruby/#{abi_version}/bin/rake")
-        end
-      end
-    end
-  end
 end
