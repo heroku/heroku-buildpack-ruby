d8b95bbaac0b2851f4067d32293850eea16de9cd
Schneems <richard.schneeman+foo@gmail.com>
2025-04-28 11:30:02 -0500

    Exercise --update behavior

--- spec/hatchet/getting_started_spec.rb
+++ spec/hatchet/getting_started_spec.rb
@@ -1,14 +1,23 @@
 require_relative '../spec_helper'
 
 describe "Heroku ruby getting started" do
-  it "clears runtime cache" do
-    Hatchet::Runner.new("ruby-getting-started").deploy do |app|
+  it "works on Heroku-24" do
+    Hatchet::Runner.new("ruby-getting-started", stack: "heroku-24").deploy do |app|
+      # Assert sprockets build cache not present on runtime
       expect(app.run("ls tmp/cache/assets")).to_not match("sprockets")
+
+      # Re-deploy with cache
+      run!("git commit --allow-empty -m empty")
+      app.push!
+      expect(app.run("which ruby").strip).to eq("/app/bin/ruby")
     end
   end
 
-  it "works on Heroku-24" do
-    Hatchet::Runner.new("ruby-getting-started", stack: "heroku-24").deploy do |app|
+  it "works on Heroku-22" do
+    Hatchet::Runner.new("ruby-getting-started", stack: "heroku-22").deploy do |app|
+      # Re-deploy with cache
+      run!("git commit --allow-empty -m empty")
+      app.push!
       expect(app.run("which ruby").strip).to eq("/app/bin/ruby")
     end
   end
--- spec/hatchet/rubies_spec.rb
+++ spec/hatchet/rubies_spec.rb
@@ -68,23 +68,5 @@ describe "Upgrading ruby apps" do
     end
   end
 
-  it "works when changing versions on heroku-22" do
-    version = "3.3.1"
-    expect(DEFAULT_STACK).to_not eq("herou-22")
-    expect(version).to_not eq(LanguagePack::RubyVersion::DEFAULT_VERSION_NUMBER)
-    app = Hatchet::Runner.new("default_ruby", stack: "heroku-22")
-    app.deploy do |app|
-      # default version
-      expect(app.run("env | grep MALLOC_ARENA_MAX")).to match("MALLOC_ARENA_MAX=2")
-      expect(app.run("env | grep DISABLE_SPRING")).to match("DISABLE_SPRING=1")
-
-      # Deploy again
-      run!(%Q{echo "ruby '#{version}'" >> Gemfile})
-      run!("git add -A; git commit -m update-ruby")
-      app.push!
-      expect(app.output).to match(version)
-      expect(app.run("ruby -v")).to match(version)
-      expect(app.output).to match("Ruby version change detected")
-    end
-  end
 end
+
