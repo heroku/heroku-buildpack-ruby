4bb28546df4a3d7d4e4ae6dc356865017419cab2
Richard Schneeman <rschneeman@salesforce.com>
2025-04-28 13:16:46 -0500

    Fix `cp` update none on Heroku 20 (#1586)
    * Fix `cp` update none on Heroku 20
    
    In https://github.com/heroku/heroku-buildpack-ruby/pull/1583 a warning was bypassed by switching from `-n` to `--update none`. Unfortunately this triggers an error with the version of `cp` on `heroku-20` (which is EOL in 2 days) due to not having that flag:
    
    ```
    -----> Preparing app for Rails asset pipeline
    remote: cp: option '--update' doesn't allow an argument
    remote: Try 'cp --help' for more information.
    ```
    
    Internal ticket link https://heroku.support/1570694.
    
    * Check exit status of copy command
    
    * Changelog

--- lib/language_pack/cache.rb
+++ lib/language_pack/cache.rb
@@ -58,7 +58,12 @@ class LanguagePack::Cache
     return unless @cache_base
 
     dest ||= path
-    copy (@cache_base + path), dest, '-a --update=none'
+
+    if ENV["STACK"] == "heroku-20"
+      copy (@cache_base + path), dest, "-a -n"
+    else
+      copy (@cache_base + path), dest, "-a --update=none"
+    end
   end
 
   # copy cache contents
@@ -69,7 +74,9 @@ class LanguagePack::Cache
 
     return false unless File.exist?(from)
     FileUtils.mkdir_p File.dirname(to)
-    system("cp #{options} #{from}/. #{to}")
+    command = "cp #{options} #{from}/. #{to}"
+    system(command)
+    raise "Command failed `#{command}`" unless $?
   end
 
   # copy contents between to places in the cache
