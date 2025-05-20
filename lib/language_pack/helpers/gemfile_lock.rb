e634ee1dcf2e491352ab2dcefdbbf982a13c2a86
Schneems <richard.schneeman+foo@gmail.com>
2025-05-20 14:05:23 -0500

    Introduce GemfileLock helper to parse Gemfile.lock
    Parses RUBY VERSION and BUNDLED WITH version from the Gemfile.lock. This information can later be used to install a Ruby version and Bundler version without needing to run arbitrary code (i.e. run `bundle platform --ruby`), which can fail depending on the combination of versions:
    
    - Bootstrap (buildpack) Ruby version
    - Customer Bundler version
    - Customer Ruby version
    
    For example, when the bootstrap version was raised to Ruby 3.3.x, It can no longer run `bundle platform --ruby` using bundler 1.x because bundler 1.x is incompatible with Ruby 3.3.

---
+++ lib/language_pack/helpers/gemfile_lock.rb
@@ -0,0 +1,78 @@
+require "language_pack/shell_helpers"
+
+module LanguagePack
+  module Helpers
+    # Centralize logic for extracting information from the `Gemfile.lock` format
+    #
+    # - Extracts Ruby version from `RUBY VERSION`
+    # - Extracts Bundler version from `BUNDLED WITH`
+    class GemfileLock
+      attr_reader :ruby, :bundler
+
+      def initialize(contents: )
+        @ruby = RubyVersionParse.new(contents: contents)
+        @bundler = BundlerVersionParse.new(contents: contents)
+      end
+
+      # Holds information about the RUBY VERSION of the parsed Gemfile.lock
+      class RubyVersionParse
+        # Ruby version from Gemfile.lock i.e. `3.3.8`
+        # Either 3 numbers or nil
+        attr_reader :ruby_version,
+          # Contains pre-release info
+          # - String: i.e. ".rc2" is a prerelease
+          # - nil: No pre-release (or no version at all)
+          :pre,
+          # Either :mri or :jruby
+          :engine,
+          # `engine_version` is the Jruby version or for MRI it is the same as `ruby_version`
+          # i.e. `<major>.<minor>.<patch>`
+          :engine_version
+
+        def initialize(contents: )
+
+          if match = contents.match(/^RUBY VERSION(\r?\n)   ruby (?<version>\d+\.\d+\.\d+)((\-|\.)(?<pre>\S*\d+))?/m)
+            @pre = match[:pre]
+            @empty = false
+            @ruby_version = match[:version]
+          else
+            @pre = nil
+            @empty = true
+            @ruby_version = nil
+          end
+
+          if jruby = contents.to_s.match(/\(jruby (?<version>(\d+|\.)+)\)/)
+            @engine = :jruby
+            @engine_version = jruby[:version]
+          else
+            @engine = :mri
+            @engine_version = ruby_version
+          end
+        end
+
+        def empty?
+          @empty
+        end
+      end
+
+      class BundlerVersionParse
+        # Bundler value from `Gemfile.lock` (String or nil) i.e. `2.5.23`
+        attr_reader :version
+
+        def initialize(contents: )
+          if match = contents.match(/^BUNDLED WITH$(\r?\n)   (?<version>(?<major>\d+)\.(?<minor>\d+)\.\d+)/m)
+            @empty = false
+            @version = match[:version]
+          else
+            @empty = true
+            @version = nil
+          end
+        end
+
+        def empty?
+          @empty
+        end
+      end
+    end
+  end
+end
---
+++ spec/helpers/gemfile_lock_spec.rb
@@ -0,0 +1,91 @@
+require "spec_helper"
+
+describe LanguagePack::Helpers::GemfileLock do
+  it "Parses empty gemfile without error" do
+    gemfile_lock = LanguagePack::Helpers::GemfileLock.new(
+      contents: <<~EOF
+      EOF
+    )
+    expect(gemfile_lock.ruby.ruby_version).to eq(nil)
+    expect(gemfile_lock.ruby.pre).to eq(nil)
+    expect(gemfile_lock.ruby.engine).to eq(:mri)
+    expect(gemfile_lock.ruby.empty?).to eq(true)
+    expect(gemfile_lock.ruby.engine_version).to eq(nil)
+
+    expect(gemfile_lock.bundler.version).to eq(nil)
+    expect(gemfile_lock.bundler.empty?).to eq(true)
+  end
+
+  it "Captures MRI version and ignores patchlevel" do
+    gemfile_lock = LanguagePack::Helpers::GemfileLock.new(
+      contents: <<~EOF
+        RUBY VERSION
+           ruby 3.3.5p100
+
+        BUNDLED WITH
+           2.3.4
+      EOF
+    )
+    expect(gemfile_lock.ruby.ruby_version).to eq("3.3.5")
+    expect(gemfile_lock.ruby.pre).to eq(nil)
+    expect(gemfile_lock.ruby.engine).to eq(:mri)
+    expect(gemfile_lock.ruby.empty?).to eq(false)
+    expect(gemfile_lock.ruby.engine_version).to eq("3.3.5")
+
+    expect(gemfile_lock.bundler.version).to eq("2.3.4")
+    expect(gemfile_lock.bundler.empty?).to eq(false)
+  end
+
+  it "captures jruby version" do
+    gemfile_lock = LanguagePack::Helpers::GemfileLock.new(
+      contents: <<~EOF
+        GEM
+          remote: https://rubygems.org/
+          specs:
+        PLATFORMS
+          java
+        RUBY VERSION
+           ruby 2.5.7p001 (jruby 9.2.13.0)
+      EOF
+    )
+    expect(gemfile_lock.ruby.ruby_version).to eq("2.5.7")
+    expect(gemfile_lock.ruby.pre).to eq(nil)
+    expect(gemfile_lock.ruby.engine).to eq(:jruby)
+    expect(gemfile_lock.ruby.empty?).to eq(false)
+    expect(gemfile_lock.ruby.engine_version).to eq("9.2.13.0")
+  end
+
+  it "handles rc dot syntax" do
+    gemfile_lock = LanguagePack::Helpers::GemfileLock.new(
+      contents: <<~EOF
+        RUBY VERSION
+           ruby 3.4.0.rc1
+
+        BUNDLED WITH
+           2.3.4
+      EOF
+    )
+    expect(gemfile_lock.ruby.ruby_version).to eq("3.4.0")
+    expect(gemfile_lock.ruby.pre).to eq("rc1")
+    expect(gemfile_lock.ruby.engine).to eq(:mri)
+    expect(gemfile_lock.ruby.empty?).to eq(false)
+    expect(gemfile_lock.ruby.engine_version).to eq("3.4.0")
+  end
+
+  it "handles preview dot syntax" do
+    gemfile_lock = LanguagePack::Helpers::GemfileLock.new(
+      contents: <<~EOF
+        RUBY VERSION
+           ruby 3.4.0.preview2
+
+        BUNDLED WITH
+           2.3.4
+      EOF
+    )
+    expect(gemfile_lock.ruby.ruby_version).to eq("3.4.0")
+    expect(gemfile_lock.ruby.pre).to eq("preview2")
+    expect(gemfile_lock.ruby.engine).to eq(:mri)
+    expect(gemfile_lock.ruby.empty?).to eq(false)
+    expect(gemfile_lock.ruby.engine_version).to eq("3.4.0")
+  end
+end
