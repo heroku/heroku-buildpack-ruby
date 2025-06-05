bec38d1453c6f60046496420f74d9e739e75800e
Richard Schneeman <rschneeman@salesforce.com>
2025-06-05 17:06:41 -0500

    Introduce `bundle list` based parser (#1610)
    * Introduce `bundle list` based parser
    
    This allows us to determine what versions of gems an application is using after `bundle install` has run. Currently, the buildpack works by loading bundler internals into memory and using those to parse the `Gemfile.lock`. That approach introduces strong compatibility requirements for coupling between Ruby versions and bundler versions.
    
    Additionally, recent versions of `bundle install` no longer output the list of gems installed:
    
    ```
    $ bundle _2.3.27_ install
    Using rake 13.2.1
    Using base64 0.2.0
    ...
    Using toml-rb 4.0.0
    Bundle complete! 11 Gemfile dependencies, 31 gems now installed.
    Use `bundle info [gemname]` to see where a bundled gem is installed.
    ```
    
    ```
    $ bundle _2.4.22_ install
    Bundle complete! 11 Gemfile dependencies, 31 gems now installed.
    Use `bundle info [gemname]` to see where a bundled gem is installed.
    ```
    
    It's helpful to have a list of installed gems on build output for debugging purposes. We can use this `bundle list` output as a proxy for the old `bundle install` output.
    
    * Run `bundle list` to list installed gems
    
    Stream `bundle list` when `bundle install` emits no gem information. This output is retained and parsed to build a list of Ruby dependencies. This will eventually replace the bundler internals that are currently in use.

---
+++ lib/language_pack/helpers/bundle_list.rb
@@ -0,0 +1,84 @@
+require "language_pack/shell_helpers"
+
+module LanguagePack::Helpers
+  class BundleList
+    class CmdError < BuildpackError
+      def initialize(command:, output:)
+        super(<<~EOF)
+          Error detecting dependencies
+
+          The Ruby buildpack requires information about your application’s dependencies to
+          complete the build. Without this information, the Ruby buildpack cannot continue.
+
+          Command failed: `#{command}`
+
+          #{output}
+        EOF
+      end
+    end
+
+    # Runs `bundle list` and builds a BundleList object
+    # from the output
+    #
+    # Announces the run, optionally streams results to the user.
+    # This is useful for the case where bundler doesn't print
+    # any version info.
+    #
+    # Example:
+    #
+    #    BundleList::Command.new(
+    #      stream_to_user: stream_to_user
+    #    ).call
+    class HumanCommand
+      include LanguagePack::ShellHelpers
+      private attr_reader :stream_to_user, :io
+
+      def initialize(stream_to_user:, io: self)
+        @io = io
+        @stream_to_user = stream_to_user
+      end
+
+      def call
+        command = "bundle list"
+        io.puts "Running: #{command}"
+
+        output = if stream_to_user
+          io.pipe(command, user_env: true)
+        else
+          io.run(command, user_env: true)
+        end
+
+        if $?.success?
+          BundleList.new(
+            output: output
+          )
+        else
+          raise CmdError.new(output: output, command: command)
+        end
+      end
+    end
+
+    def initialize(output: )
+      @raw = output
+      @gems = {}
+      @raw.scan(/\* (?<name>\S+) \((?<version>[a-zA-Z0-9\.]+)(?<git_sha> [a-zA-Z0-9]+)?\)/) do
+        captures = Regexp.last_match.named_captures
+        @gems[captures["name"]] = captures["version"]
+      end
+    end
+
+    def has_gem?(name)
+      @gems[name]
+    end
+
+    def length
+      @gems.length
+    end
+
+    def gem_version(name)
+      if version = @gems[name]
+        Gem::Version.new(version)
+      end
+    end
+  end
+end
