e5d51e92f85a83936386974bc962b1fc9a583b53
Schneems <richard.schneeman+foo@gmail.com>
2025-08-14 16:50:59 -0500

    Introduce bash based metrics

---
+++ bin/support/metrics.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# File contains functions for storing metrics from the buildpack in bash.
+#
+# The format of the report file is line separated key-value pairs.
+#
+# Example:
+#   ruby_version: "3.3.3"
+#   ruby_install_duration: 1.234
+#
+# All keys get `ruby.` prepended to them in the backend automatically.
+
+set -euo pipefail
+
+# Variables shared by this whole module
+BASH_REPORT_FILE=""
+
+# Must be called before you can use any other methods
+metrics::init() {
+	local cache_dir="${1}"
+	BASH_REPORT_FILE="${cache_dir}/.heroku/ruby/bash_build_report.yml"
+}
+
+# This should be called after metrics::init in bin/compile
+metrics::clear() {
+	mkdir -p "$(dirname "${BASH_REPORT_FILE}")"
+	echo "" > "${BASH_REPORT_FILE}"
+}
+
+# Adds a key-value pair to the report file without any attempt to quote or escape the value.
+metrics::kv_raw() {
+	local key="${1}"
+	local value="${2}"
+	if [[ -n "${value}" ]]; then
+		echo "${key}: ${value}" >> "${BASH_REPORT_FILE}"
+	fi
+}
+
+# Adds a key-value pair to the report file, quoting the value.
+metrics::kv_string() {
+	local key="${1}"
+	local value="${2}"
+	if [[ -n "${value}" ]]; then
+		metrics::kv_raw "$key" "'${value//\'/\'\'}'"
+	fi
+}
+
+# Adds a key=duration to the report file
+#
+# Example:
+#   start_time=$(nowms)
+#   sleep 1
+#   metrics::k_duration_since "ruby_install" "${start_time}"
+#
+#   metrics::print
+#   # => ruby_install: 1.234
+metrics::k_duration_since() {
+	local key="${1}"
+	local start="${2}"
+	local end="${3:-$(nowms)}"
+	local time
+	time="$(echo "${start}" "${end}" | awk '{ printf "%.3f", ($2 - $1)/1000 }')"
+	metrics::kv_raw "$key" "${time}"
+}
+
+# Does what it says on the tin.
+metrics::print() {
+	cat "${BASH_REPORT_FILE}"
+}
