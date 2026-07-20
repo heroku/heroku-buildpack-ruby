0d239735f7e8ff38b32ed8efaa5d0c15d518b9ef
Richard Schneeman <rschneeman@salesforce.com>
2026-07-20 10:12:09 -0500

    Disable Lint/InterpolationCheck for literal rails runner command
    standard 1.55.0 (rubocop 1.87.0) added Lint/InterpolationCheck, which
    flags the single-quoted expected command strings in the RailsRunner spec.
    These strings must literally contain `#{...}` because they are the raw
    command sent to the `rails runner` subprocess, so converting to double
    quotes would break the assertion. Disable the cop inline instead.

--- spec/helpers/rails_runner_spec.rb
+++ spec/helpers/rails_runner_spec.rb
@@ -20,12 +20,12 @@ describe "Rails Runner" do
       rails_runner = LanguagePack::Helpers::RailsRunner.new
       rails_runner.detect("active_storage.service")
 
-      expected = 'rails runner "begin; puts %Q{heroku.detecting.config.for.active_storage.service=#{Rails.application.config.try(:active_storage).try(:service)}}; rescue => e; end;"'
+      expected = 'rails runner "begin; puts %Q{heroku.detecting.config.for.active_storage.service=#{Rails.application.config.try(:active_storage).try(:service)}}; rescue => e; end;"' # standard:disable Lint/InterpolationCheck
       expect(rails_runner.command).to eq(expected)
 
       rails_runner.detect("assets.compile")
 
-      expected = 'rails runner "begin; puts %Q{heroku.detecting.config.for.active_storage.service=#{Rails.application.config.try(:active_storage).try(:service)}}; rescue => e; end; begin; puts %Q{heroku.detecting.config.for.assets.compile=#{Rails.application.config.try(:assets).try(:compile)}}; rescue => e; end;"'
+      expected = 'rails runner "begin; puts %Q{heroku.detecting.config.for.active_storage.service=#{Rails.application.config.try(:active_storage).try(:service)}}; rescue => e; end; begin; puts %Q{heroku.detecting.config.for.assets.compile=#{Rails.application.config.try(:assets).try(:compile)}}; rescue => e; end;"' # standard:disable Lint/InterpolationCheck
       expect(rails_runner.command).to eq(expected)
     end
   end
